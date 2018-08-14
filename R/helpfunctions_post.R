666ad7df5dfa622520a8a1a4b8547b98ac1031a3
N.S. Erler <n.erler@erasmusmc.nl>
2018-08-14 16:37:18 +0200

    bugfix (as.list(NULL) is not NULL...)

--- R/helpfunctions_post.R
+++ R/helpfunctions_post.R
@@ -47,27 +47,27 @@
 get_subset <- function(object, subset, call_orig, warn = TRUE) {
   subset <- as.list(subset)
 
-  if (is.null(subset) & !as.list(object$monitor_params)$analysis_main)
+  if (length(subset) == 0 & !as.list(object$monitor_params)$analysis_main)
     return(object$MCMC)
 
-  if (is.null(subset) & as.list(object$monitor_params)$analysis_main)
+  if (length(subset) == 0 & as.list(object$monitor_params)$analysis_main)
     subset <- c(analysis_main = TRUE)
 
-  if (!is.null(subset) && is.null(as.list(subset)$analysis_main))
-    subset$analysis_main = FALSE
+  if (!length(subset) == 0 && is.null(as.list(subset)$analysis_main))
+    subset$analysis_main <- FALSE
 
   s <- do.call(get_params, c(object, object$Mlist, subset))
 
   repl <- sapply(s, function(r) {
-    if(grepl("^beta$", r)) {
+    if (grepl("^beta$", r)) {
       get_coef_names(object$Mlist, object$K)[, 2]
-    } else if(!r %in% colnames(object$MCMC[[1]]) & any(grepl(r, colnames(object$MCMC[[1]])))) {
+    } else if (!r %in% colnames(object$MCMC[[1]]) & any(grepl(r, colnames(object$MCMC[[1]])))) {
       grep(paste0(r, "\\[[[:digit:]]+\\]"), colnames(object$MCMC[[1]]), value = TRUE)
     }
   }, simplify = FALSE)
 
   for (i in seq_along(repl)) {
-    if(!is.null(repl[[i]])) {
+    if (!is.null(repl[[i]])) {
       s <- append(s, repl[[i]], after = match(names(repl)[i], s))
       s <- s[-match(names(repl)[i], s)]
     }
