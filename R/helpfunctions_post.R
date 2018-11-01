b042e69c6ac4757f4fa00a90e551736aabe5ccaf
N.S. Erler <n.erler@erasmusmc.nl>
2018-11-01 08:13:00 +0100

    deleted old code

--- R/helpfunctions_post.R
+++ R/helpfunctions_post.R
@@ -1,48 +1,3 @@
-# get_subset <- function(subset, MCMC, object) {
-#   if (any(subset != "analysis_main")) {
-#     subset <- subset[which(subset != "analysis_main")]
-#
-#     if (inherits(MCMC, "mcmc.list")) {
-#       subset <- sapply(subset, function(i) {
-#         if (is.na(match(i, colnames(MCMC[[1]])))) {
-#           colnames(MCMC[[1]])[as.numeric(i)]
-#         } else {
-#           i
-#         }
-#       })
-#     } else {
-#       subset <- sapply(subset, function(i) {
-#         if (is.na(match(i, colnames(MCMC))) & i != "analysis_main") {
-#           colnames(MCMC)[as.numeric(i)]
-#         } else {
-#           i
-#         }
-#       })
-#     }
-#   }
-#
-#   if (any(subset == "analysis_main")) {
-#     coefs <- get_coef_names(object$Mlist, object$K)
-#
-#     subset <- append(subset[which(subset != "analysis_main")], coefs[, 2], after = 0)
-#     if (!is.null(object$Mlist$auxvars))
-#       subset <- subset[-which(subset %in% c(object$Mlist$auxvars,
-#                                             unlist(lapply(object$Mlist$refs[object$Mlist$auxvars],
-#                                                           attr, "dummies"))))]
-#
-#     if (object$analysis_type == "lm" |
-#         (object$analysis_type == "glm" &
-#          attr(object$analysis_type, "family") %in% c("Gamma", "gaussian"))) {
-#       subset <- c(subset, paste0("sigma_", names(object$Mlist$y)))
-#     }
-#     if (object$analysis_type == "lme") {
-#       subset <- c(subset,
-#                   paste0("sigma_", names(object$Mlist$y)),
-#                   grep("^D\\[[[:digit:]]*,[[:digit:]]*\\]", colnames(MCMC), value = TRUE))
-#     }
-#   }
-#   return(MCMC[, subset, drop = FALSE])
-# }
 
 get_subset <- function(object, subset, call_orig, keep_aux = FALSE, warn = TRUE) {
   subset <- as.list(subset)
@@ -79,15 +34,6 @@ get_subset <- function(object, subset, call_orig, keep_aux = FALSE, warn = TRUE)
 
   if (length(sub) == 0)
     sub <- colnames(object$MCMC[[1]])
-  # stop(gettextf("None of the parameters selected by 'subset' is in the MCMC sample %s." ,
-  #               dQuote(call_orig$object)), call. = FALSE)
-
-  # if (any(!s %in% colnames(object$MCMC[[1]]))) {
-  #   if (warn)
-  #   warning(paste0("No MCMC samples found for ",
-  #                  paste("'", s[!s %in% colnames(object$MCMC[[1]])], "'",
-  #                        sep = "", collapse = ", "), "."), immediate. = TRUE)
-  # }
 
   return(object$MCMC[, sub, drop = FALSE])
 }
