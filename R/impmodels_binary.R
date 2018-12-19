af3b1fc2c3b9f90f5b5733357343cb0439ad9e10
N.S. Erler <n.erler@erasmusmc.nl>
2018-12-19 12:18:10 +0100

    add ppc and change p to mu

--- R/impmodels_binary.R
+++ R/impmodels_binary.R
@@ -5,7 +5,7 @@
 # @param par_elmts elements of the parameter vector to be used
 # @param par_name name of the parameter
 # @export
-impmodel_logit <- function(varname, dest_col, Xc_cols, par_elmts, par_name, ...){
+impmodel_logit <- function(varname, dest_col, Xc_cols, par_elmts, par_name, ppc, ...){
 
   if (length(Xc_cols) != length(par_elmts)) {
     stop("The size of the design matrix and length of parameter vector do not match!")
@@ -16,9 +16,20 @@ impmodel_logit <- function(varname, dest_col, Xc_cols, par_elmts, par_name, ...)
                                 parelmts = par_elmts,
                                 cols = Xc_cols, indent = indent)
 
-  paste0(tab(4), "# logistic model for ", varname,"\n",
-         tab(4), "Xc[i, ", dest_col, "] ~ dbern(max(1e-7, min(1-1e-7, p_", varname, "[i])))", "\n",
-         tab(4), "logit(p_", varname, "[i]) <- ", predictor, "\n\n")
+
+  paste_ppc <- if (ppc) {
+    paste0("\n\n",
+           tab(4), '# Posterior predictive check for ', varname, '\n',
+           tab(4), varname, "_ppc[i] ~ dbern(max(1e-7, min(1-1e-7, mu_", varname, "[i])))", "\n"
+    )
+  }
+
+
+  paste0('\n',
+         tab(4), "# logistic model for ", varname,"\n",
+         tab(4), "Xc[i, ", dest_col, "] ~ dbern(max(1e-7, min(1-1e-7, mu_", varname, "[i])))", "\n",
+         tab(4), "logit(mu_", varname, "[i]) <- ", predictor,
+         paste_ppc, "\n")
 }
 
 
