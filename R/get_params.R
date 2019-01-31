c3aa26db6104de2d30a1e1e56b406d1271d29026
N.S. Erler <n.erler@erasmusmc.nl>
2019-01-31 10:11:28 +0100

    simplify arguments (use Mlist) and enable monitoring of incomplete longitudinal variables

--- R/get_params.R
+++ R/get_params.R
@@ -1,30 +1,5 @@
-# Get parameters to follow
-# @param meth imputation method
-# @param analysis_type analysis model type
-# @param y_name name of the outcome variable
-# @param nranef number of columns in random effects design matrix
-# @param Xc matrix
-# @param Xtrafo matrix
-# @param Xcat matrix
-# @param analysis_main logical
-# @param analysis_random logical
-# @param imp_pars logical
-# @param betas logical
-# @param tau_y logical
-# @param sigma_y logical
-# @param ranef logical
-# @param invD logical
-# @param D logical
-# @param RinvD logical
-# @param alphas logical
-# @param tau_imp logical
-# @param gamma_imp logical
-# @param delta_imp logical
-# @param shaperate_imp logical
-# @param imps logical
-# @export
-get_params <- function(models, analysis_type, family,
-                       Xc, Xcat, Xtrafo, y_name = NULL, y = NULL, nranef = NULL,
+
+get_params <- function(models, analysis_type, family, Mlist,
                        imp_par_list = NULL,
                        analysis_main = TRUE,
                        analysis_random = FALSE,
@@ -37,9 +12,8 @@ get_params <- function(models, analysis_type, family,
                        alphas = NULL, tau_imp = NULL, gamma_imp = NULL,
                        delta_imp = NULL, other = NULL, ...){
 
-  if (is.null(y_name)) {
-    y_name <- names(y)
-  }
+  y_name <- colnames(Mlist$y)
+
   if (missing(family))
     family <- attr(analysis_type, "family")
 
@@ -61,7 +35,7 @@ get_params <- function(models, analysis_type, family,
     if (is.null(ranef)) ranef <- TRUE
     if (is.null(invD)) invD <- TRUE
     if (is.null(D)) D <- TRUE
-    if (is.null(RinvD) & nranef > 1) RinvD <- TRUE
+    if (is.null(RinvD) & Mlist$nranef > 1) RinvD <- TRUE
   }
 
   if (imp_pars) {
@@ -104,11 +78,11 @@ get_params <- function(models, analysis_type, family,
   if (analysis_type %in% c("lme", "glme", "clmm")) {
     params <- c(params,
                 if (ranef) "b",
-                if (invD) unlist(sapply(1:nranef, function(x)
+                if (invD) unlist(sapply(1:Mlist$nranef, function(x)
                   paste0("invD[", 1:x, ",", x,"]"))),
-                if (D) unlist(sapply(1:nranef, function(x)
+                if (D) unlist(sapply(1:Mlist$nranef, function(x)
                   paste0("D[", 1:x, ",", x,"]"))),
-                if (RinvD) paste0("RinvD[", 1:nranef, ",", 1:nranef,"]")
+                if (RinvD) paste0("RinvD[", 1:Mlist$nranef, ",", 1:Mlist$nranef,"]")
     )
   }
 
@@ -117,11 +91,18 @@ get_params <- function(models, analysis_type, family,
       if (x$dest_mat == 'Xtrafo') x[c('dest_col', 'trafo_cols')]
     )
 
-    Xc_NA <- if (any(is.na(Xc))) which(is.na(Xc), arr.ind = TRUE)
-    Xc_NA <- Xc_NA[Xc_NA[, 2] %in% which(colSums(!is.na(Xc)) > 0), ]
-    Xcat_NA <- if (any(is.na(Xcat))) which(is.na(Xcat), arr.ind = TRUE)
-    Xtrafo_NA <- if (any(is.na(Xtrafo))) which(is.na(Xtrafo), arr.ind = TRUE)
-    if (any(is.na(Xtrafo))) {
+    Xc_NA <- if (any(is.na(Mlist$Xc))) which(is.na(Mlist$Xc), arr.ind = TRUE)
+    Xc_NA <- Xc_NA[Xc_NA[, 2] %in% which(colSums(!is.na(Mlist$Xc)) > 0), , drop = FALSE]
+
+    Xcat_NA <- if (any(is.na(Mlist$Xcat))) which(is.na(Mlist$Xcat), arr.ind = TRUE)
+    Xtrafo_NA <- if (any(is.na(Mlist$Xtrafo))) which(is.na(Mlist$Xtrafo), arr.ind = TRUE)
+
+    Xl_NA <- if (any(is.na(Mlist$Xl))) which(is.na(Mlist$Xl), arr.ind = TRUE)
+    Xl_NA <- Xl_NA[Xl_NA[, 2] %in% which(colSums(!is.na(Mlist$Xl)) > 0), , drop = FALSE]
+
+    Z_NA <- if (any(is.na(Mlist$Z))) which(is.na(Mlist$Z), arr.ind = TRUE)
+
+    if (any(is.na(Mlist$Xtrafo))) {
       Xtrafo_NA_Xc <- matrix(nrow = 0, ncol = 2)
       for (i in seq_along(repl_list)) {
         for (j in seq_along(repl_list[[i]]$trafo_cols)) {
@@ -137,6 +118,10 @@ get_params <- function(models, analysis_type, family,
     params <- c(params,
                 if (!is.null(Xc_NA))
                   paste0("Xc[", apply(Xc_NA, 1, paste, collapse = ","), "]"),
+                if (!is.null(Xl_NA))
+                  paste0("Xl[", apply(Xl_NA, 1, paste, collapse = ","), "]"),
+                if (!is.null(Z_NA))
+                  paste0("Z[", apply(Z_NA, 1, paste, collapse = ","), "]"),
                 if (!is.null(Xtrafo_NA))
                   c(paste0("Xtrafo[", apply(Xtrafo_NA, 1, paste, collapse = ","), "]"),
                     paste0("Xc[", apply(Xtrafo_NA_Xc, 1, paste, collapse = ","), "]")),
