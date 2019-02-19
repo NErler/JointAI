3119ef70ae18cc747741bd904d55d5839f2b82b3
N.S. Erler <n.erler@erasmusmc.nl>
2019-02-19 16:35:27 +0100

    function plot_imp_distr added

---
+++ R/plot_imp_distr.R
@@ -0,0 +1,102 @@
+#' Plot the distribution of observed and imputed values
+#'
+#' @param data a data.frame containing multiple imputations
+#' @param imp the name of the variable specifying the imputation indicator
+#' @param id the name of the variable specifying the subject indicator
+#' @inheritParams sharedParams
+#' @import ggplot2
+#' @export
+#'
+#'
+
+plot_imp_distr <- function(data, imp = 'Imputation_', id = 'id', ncol = NULL, nrow = NULL) {
+
+  if (!requireNamespace('ggplot2', quietly = TRUE))
+    stop("This function requires the package ggplot2 to be installed.")
+
+  if (!requireNamespace('ggpubr', quietly = TRUE))
+    stop("This function requires the package ggpubr to be installed.")
+
+  subDF <- data[, (colSums(is.na(data[data[, imp] == 0, ]) > 0) &
+                   colSums(is.na(data[data[, imp] != 0, ]) == 0)) |
+                names(data) %in% c(imp, id)]
+
+  DForig <- subDF[subDF[, imp] == 0, ]
+
+  w <- as.data.frame(is.na(DForig))
+  w[, c(imp, id)] <- DForig[, c(imp, id)]
+
+  type <- sapply(subDF, is.factor)
+
+  DFlong <- melt_data.frame(subDF, id.vars = c(imp, id))
+
+  wlong <- melt_data.frame(w, id.vars = c(imp, id), valname = 'mis')
+  wlong <- unique(wlong)
+
+
+  DFlong <- merge(DFlong, wlong, by = c('id', 'variable'),suffixes = c("",".y"))
+  DFlong$type <- ifelse(type[as.character(DFlong$variable)], 'factor', 'numeric')
+
+  plotDF <- DFlong[(DFlong[, imp] == 0 & !DFlong$mis) |
+                     (DFlong[, imp] != 0 & DFlong$mis), ]
+
+
+  p <- lapply(split(plotDF, plotDF$variable), function(dat) {
+    if (unique(dat$type) == 'factor') {
+      prop <- sapply(split(dat, dat[, imp]),
+                     function(x) prop.table(table(x$value)))
+      plong <- JointAI:::melt_matrix(prop, valname = 'proportion',
+                                     varnames = c('value', imp))
+      dat <- merge(dat, plong)
+    }
+
+    ggplot(dat) +
+      facet_wrap('variable', scales = 'free') +
+      scale_color_manual(name = '',
+                         limits = c(FALSE, TRUE),
+                         values = c('dodgerblue3', 'midnightblue'),
+                         labels = c('imputed', 'observed')) +
+      scale_fill_manual(name = '', limits = c(FALSE, TRUE),
+                        values = c('dodgerblue3', 'midnightblue'),
+                        labels = c('imputed', 'observed')) +
+      scale_size_manual(name = '',
+                        limits = c(FALSE, TRUE),
+                        values = c(0.5, 1.3),
+                        labels = c('imputed', 'observed')) +
+      xlab('') +
+      if (unique(dat$type == 'numeric')) {
+        if (min(table(dat[, imp])) == 1) {
+          stat_density(aes(x = as.numeric(value), color = get(imp) == 0,
+                           size = get(imp) == 0), geom = 'line', position = 'identity')
+        } else {
+          stat_density(aes(x = as.numeric(value),
+                           size = get(imp) == 0,
+                           color = get(imp) == 0,
+                           group = get(imp)), geom = 'line', position = 'identity')
+        }
+      } else {
+        geom_bar(aes(x = value, y = proportion, group = get(imp),
+                     fill = get(imp) == 0),
+                 position = "dodge", stat = 'identity',
+                 color = 'midnightblue')
+      }
+  })
+
+  # get number of rows and columns of plots
+  if (is.null(nrow) & is.null(ncol)) {
+    dims <- if (ncol(subDF) > 27) {
+      grDevices::n2mfrow(25)
+    } else {
+      grDevices::n2mfrow(ncol(subDF) - 2)
+    }
+  } else if (is.null(nrow) & !is.null(ncol)) {
+    dims <- c(ceiling((ncol(subDF) - 2)/ncol), ncol)
+  } else if (is.null(ncol) & !is.null(nrow)) {
+    dims <- c(nrow, ceiling((ncol(subDF) - 2)/nrow))
+  } else {
+    dims <- c(nrow, ncol)
+  }
+
+  ggpubr::ggarrange(plotlist = p, ncol = dims[2], nrow = dims[1],
+                    common.legend = TRUE, legend = "bottom")
+}
