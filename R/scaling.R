d11c6dd824733d4c15d8174f098962a4590d808e
N.S. Erler <n.erler@erasmusmc.nl>
2019-02-21 14:56:53 +0100

    bugfix: no scaling for variables with poisson model glmm_poisson

--- R/scaling.R
+++ R/scaling.R
@@ -21,13 +21,13 @@ scale_matrix <- function(X, scale_vars, scale_pars, models) {
           usecenter <- if (!k %in% names(models)) {
             TRUE
           } else {
-            !models[k] %in% c("lognorm", "gamma", "beta", 'glmm_gamma')
+            !models[k] %in% c("lognorm", "gamma", "beta", 'glmm_gamma', 'glmm_poisson')
           }
 
           usescale <- if (!k %in% names(models)) {
             TRUE
           } else {
-            !models[k] %in% c("gamma", "beta", 'glmm_gamma')
+            !models[k] %in% c("gamma", "beta", 'glmm_gamma', 'glmm_poisson')
           }
 
           xsc <- scale(X[, k], center = usecenter, scale = usescale)
