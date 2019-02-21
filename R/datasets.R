e3633ae1d7639cc9db3a1cb8e4c614c76b57d04c
N.S. Erler <n.erler@erasmusmc.nl>
2019-02-21 14:50:39 +0100

    update longDF with count variables

--- R/datasets.R
+++ R/datasets.R
@@ -6,7 +6,7 @@
 #'
 #' @usage data(longDF)
 #'
-#' @format A simulated data frame with 341 rows and 17 variables with data from 100 subjects:
+#' @format A simulated data frame with 329 rows and 21 variables with data from 100 subjects:
 #' \describe{
 #'   \item{C1}{continuous, complete baseline variable}
 #'   \item{C2}{continuous, incomplete baseline variable}
@@ -16,12 +16,16 @@
 #'   \item{M2}{unordered factor; incomplete baseline variable}
 #'   \item{O1}{ordered factor; complete baseline variable}
 #'   \item{O2}{ordered factor; incomplete baseline variable}
+#'   \item{P1}{count variable; complete baseline variable}
+#'   \item{P2}{count variable; incomplete baseline variable}
 #'   \item{c1}{continuous, complete longitudinal variable}
 #'   \item{c2}{continuous incomplete longitudinal variable}
 #'   \item{b1}{binary, complete longitudinal variable}
 #'   \item{b2}{binary incomplete longitudinal variable}
-#'   \item{c1}{ordered factor; complete longitudinal variable}
-#'   \item{c2}{ordered factor; incomplete longitudinal variable}
+#'   \item{o1}{ordered factor; complete longitudinal variable}
+#'   \item{o2}{ordered factor; incomplete longitudinal variable}
+#'   \item{p1}{count variable; complete longitudinal variable}
+#'   \item{p2}{count variable; incomplete longitudinal variable}
 #'   \item{id}{id (grouping) variable}
 #'   \item{time}{continuous complete longitudinal variable}
 #'   \item{y}{continuous, longitudinal (outcome) variable}
@@ -34,9 +38,10 @@
 
 # longDF <- sim_data(N = 100, norm = c("C1", "C2"), bin = c("B1", "B2"),
 #                    multi = c('M1', "M2"), ord = c("O1", "O2"),
+#                    count = c('P1', 'P2'),
 #                    longnorm = c('c1', "c2"), longbin = c("b1", 'b2'),
-#                    longord = c('o1', 'o2'),
-#                    misvar = c('C2', 'B2', "M2", 'O2', 'c2', "b2", 'o2'),
+#                    longord = c('o1', 'o2'), longcount = c('p1', 'p2'),
+#                    misvar = c('C2', 'B2', "M2", 'O2', 'P2', 'c2', "b2", 'o2', 'p2'),
 #                    seed = 2019)[[2]]
 
 #' Cross-sectional example dataset
