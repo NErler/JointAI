25cd328790f31a0e76661c60b55236a9710e7ca3
N.S. Erler <n.erler@erasmusmc.nl>
2019-07-12 17:51:22 +0200

    plots posterior predictive check (first, unfinished version)

--- R/plots_ppc.R
+++ R/plots_ppc.R
@@ -1,65 +1,308 @@
-# plot_ppc <- function(object, fun, var, type = c('histogram', 'density', 'chainwise_dens'), ...) {
-#
-#   type <- match.arg(type)
-#
-#   if (var %in% names(object$Mlist$refs))
-#     var_dum <- attr(object$Mlist$refs[[var]], 'dummies')
-#   else
-#     var_dum <- var
-#
-#   var_rep <- object$MCMC[, grep(paste0(var, "_ppc\\["), colnames(object$MCMC[[1]]))]
-#
-#   if (object$analysis_type == 'survreg' & var == colnames(object$Mlist$y)) {
-#     obs <- object$data_list$ctime
-#     cens <- ifelse(object$data_list$cens == 1, 'censored', 'event observed')
-#   } else {
-#     obs <- if (var_dum %in% names(object$data_list)) {
-#       object$data_list[[var_dum]]
-#     } else if (var_dum %in% colnames(object$data_list$Xc)) {
-#                 c(object$data_list$Xc[, var_dum])
-#     }
-#     cens <- 0
-#   }
-#
-#   value_obs <- if (any(c('...', 'na.rm') %in% names(formals(fun))))
-#     reshape2::melt(lapply(split(obs, cens), fun, na.rm = TRUE))
-#   else
-#     reshape2::melt(lapply(split(obs, cens), fun))
-#
-#   names(value_obs) <- gsub('L1', 'group', names(value_obs))
-#
-#   l <- lapply(var_rep, apply, MARGIN = 1, function(x) {
-#     lapply(split(x, cens), fun)
-#   })
-#
-#   names(l) <- as.character(1:length(l))
-#   value_rep <- reshape2::melt(l)
-#   names(value_rep) <- gsub('L1', 'chain', names(value_rep))
-#   names(value_rep) <- gsub('L2', 'iteration', names(value_rep))
-#   names(value_rep) <- gsub('L3', 'group', names(value_rep))
-#
-#
-#   p <- ggplot2::ggplot(value_rep, ggplot2::aes(x = value)) +
-#     ggplot2::xlab(var)
-#
-#   if (type == 'histogram')
-#     p <- p + ggplot2::geom_histogram(...)
-#   if (type == 'density')
-#     p <- p + ggplot2::geom_density(...)
-#   if (type == 'chainwise_dens')
-#     p <- p + ggplot2::geom_density(ggplot2::aes(color = chain), ...)
-#
-#
-#   p <- p + ggplot2::geom_vline(data = value_obs, ggplot2::aes(xintercept = value))
-#
-#   if (object$analysis_type == 'survreg')
-#     print(p + ggplot2::facet_wrap('group'))
-#   else print(p)
-#
-#   invisible(list(value_rep = value_rep, value_obs = value_obs))
+#' Plot a posterior predictive summary
+#'
+#' @inheritParams sharedParams
+#' @param fun the function to be used to summarize the sample
+#' @param vars optional; names of the variables for which the posterior predictive summary should be plotted
+#' @param type type of the plot; one of \code{"histogram"}, \code{"density"} or \code{"chainwise_density"} (may be abbreviated)
+#' @param ... additional parameters passed to the geom that is used
+#' @export
+
+ppc_summary <- function(object, fun, vars = NULL, type = c('histogram', 'density', 'chainwise_dens'), ...) {
+
+  if (!object$Mlist$ppc)
+    stop(paste0("A posterior predictive check requires the fitted values to ",
+                "be monitored (set the argument ", dQuote('ppc = TRUE'), ")."))
+
+  type <- match.arg(type)
+
+  if (is.null(vars)) {
+    vars <- c(names(object$models), colnames(object$Mlist$y))
+  }
+
+  dat <- sapply(vars, function(var) {
+
+    if (var %in% names(object$Mlist$refs))
+      var_dum <- attr(object$Mlist$refs[[var]], 'dummies')
+    else
+      var_dum <- var
+
+    var_rep <- object$MCMC[, grep(paste0(var, "_ppc\\["), colnames(object$MCMC[[1]]))]
+
+    if (object$analysis_type == 'survreg' & var == colnames(object$Mlist$y)) {
+      obs <- object$data_list$ctime
+      cens <- ifelse(object$data_list$cens == 1, 'censored', 'event observed')
+    } else {
+      obs <- if (var %in% names(object$data_list)) {
+        c(na.omit(object$data_list[[var]]))
+      } else if (var %in% object$Mlist$trafos$var) {
+        c(na.omit(object$data_list$Xtrafo[, var]))
+      } else if (var %in% names(object$Mlist$refs) &
+                 length(levels(object$Mlist$refs[[var]])) > 2) {
+        c(na.omit(object$data_list$Xcat[, var]))
+      } else if (var_dum %in% colnames(object$data_list$Xc)) {
+        c(na.omit(object$data_list$Xc[, var_dum]))
+      }
+      cens <- 0
+    }
+
+    value_obs <- #if (any(c('...', 'na.rm') %in% names(formals(fun))))
+      # reshape2::melt(lapply(split(obs, cens), fun, na.rm = TRUE))
+      melt_list(lapply(split(obs, cens), fun), varnames = 'group')
+    # else
+    #   reshape2::melt(lapply(split(obs, cens), fun))
+
+    # names(value_obs) <- gsub('L1', 'group', names(value_obs))
+
+    l <- lapply(var_rep, apply, MARGIN = 1, function(x) {
+      lapply(split(x, cens), fun)
+    })
+
+    names(l) <- as.character(1:length(l))
+    # value_rep <- reshape2::melt(l)
+    # names(value_rep) <- gsub('L1', 'chain', names(value_rep))
+    # names(value_rep) <- gsub('L2', 'iteration', names(value_rep))
+    # names(value_rep) <- gsub('L3', 'group', names(value_rep))
+
+    value_rep <- melt_list(l)
+    names(value_rep) <- gsub('L3', 'chain', names(value_rep))
+    names(value_rep) <- gsub('L2', 'iteration', names(value_rep))
+    names(value_rep) <- gsub('L1', 'group', names(value_rep))
+
+    list(rep = value_rep, obs = as.data.frame(value_obs))
+  }, simplify = FALSE)
+
+
+  dat_obs <- melt_list(lapply(dat, "[[", 'obs'))
+  names(dat_obs) <- gsub("L2", 'var', names(dat_obs))
+  dat_rep <- melt_list(lapply(dat, "[[", 'rep'))
+  names(dat_rep) <- gsub("L1", 'var', names(dat_rep))
+
+
+  p <- ggplot2::ggplot(dat_rep, ggplot2::aes(x = value))
+    # ggplot2::xlab(var) +
+    # ggplot2::facet_wrap('var', scales = 'free')
+
+  if (type == 'histogram')
+    p <- p + ggplot2::geom_histogram(...)
+  if (type == 'density')
+    p <- p + ggplot2::stat_density(geom = 'line', ...)
+  if (type == 'chainwise_dens')
+    p <- p + ggplot2::stat_density(ggplot2::aes(color = chain), geom = 'line', ...)
+
+
+  p <- p + ggplot2::geom_vline(data = dat_obs, ggplot2::aes(xintercept = value))
+
+  if (object$analysis_type == 'survreg')
+    print(p + ggplot2::facet_wrap(group ~ var, scales = 'free'))
+  else print(p + ggplot2::facet_wrap('var', scales = 'free'))
+
+  invisible(list(rep = dat_rep, obs = dat_obs))
+}
+
+
+#' Plot a posterior predictive check
+#'
+#' @inheritParams sharedParams
+#' @param fun the function to be applied
+#' @param vars optional; names of the variables for which the posterior predictive summary should be plotted
+#' @param input type of input expected by the function; one of \code{"matrix"}
+#'              (iterations are rows, observations are columns) or \code{"scalar"}
+#'
+#' @export
+ppc_plot <- function(object, fun, vars = NULL, input = c('matrix', 'scalar')) {
+
+  input <- match.arg(input)
+
+  if (is.null(vars)) {
+    vars <- c(names(object$models), colnames(object$Mlist$y))
+  }
+
+  dat <- sapply(vars, function(var) {
+
+    if (var %in% names(object$Mlist$refs))
+      var_dum <- attr(object$Mlist$refs[[var]], 'dummies')
+    else
+      var_dum <- var
+
+
+
+    # get the replications from the MCMC sample
+    rep_list <- object$MCMC[, grep(paste0(var, "_ppc\\["),
+                                   colnames(object$MCMC[[1]]))]
+
+    # get the expected values from the MCMC sample
+    mu_list <- object$MCMC[, grep(paste0("mu_", var, "\\["),
+                                  colnames(object$MCMC[[1]]))]
+
+
+    # get the observed values from the data_list
+    if (object$analysis_type == 'survreg' & var == colnames(object$Mlist$y)) {
+      obs <- object$data_list$ctime
+      cens <- ifelse(object$data_list$cens == 1, 'censored', 'event observed')
+    } else {
+      obs <- if (var %in% names(object$data_list)) {
+        c(object$data_list[[var]])
+      } else if (var %in% object$Mlist$trafos$var) {
+        c(object$data_list$Xtrafo[, var])
+      } else if (var %in% names(object$Mlist$refs) &
+                 length(levels(object$Mlist$refs[[var]])) > 2) {
+        if (var %in% names(object$data_list$Xcat)) {
+          c(object$data_list$Xcat[, var])
+        } else {
+          c(object$data_list$Xlcat[, var])
+        }
+      } else if (var_dum %in% colnames(object$data_list$Xc)) {
+        c(object$data_list$Xc[, var_dum])
+      } else if (var_dum %in% colnames(object$data_list$Xl)) {
+        c(object$data_list$Xl[, var_dum])
+      }
+      cens <- 0
+    }
+
+    if (length(obs) != ncol(mu_list[[1]]))
+      stop("Dimension mismatch between obs and mu_list.")
+
+    obs <- matrix(obs, nrow = nrow(mu_list[[1]]), ncol = length(obs), byrow = TRUE)
+    obs_list <- replicate(length(mu_list), obs, simplify = FALSE)
+
+
+    if (input == 'matrix') {
+      mapply(fun, obs = obs_list, rep = rep_list, mu = mu_list, SIMPLIFY = FALSE)
+    } else {
+      lapply(seq_along(mu_list), function(m) {
+        sapply(1:nrow(mu_list[[m]]), function(k) {
+          fun(obs_list[[m]][k, ], rep_list[[m]][k, ], mu_list[[m]][k, ])
+        })
+      })
+    }
+  }, simplify = FALSE)
+
+  plotDF <- melt_list(dat)
+
+  ggplot2::ggplot(plotDF, ggplot2::aes(x = value, group = .data$L1, color = factor(.data$L1))) +
+    ggplot2::stat_density(geom = 'line', position = 'identity') +
+    ggplot2::facet_wrap('L2', scales = 'free')
+
+}
+
+ppc_ks <- function(object, var, exact = NULL) {
+    # get the replications from the MCMC sample
+    rep_list <- object$MCMC[, grep(paste0(var, "_ppc\\["),
+                                   colnames(object$MCMC[[1]]))]
+
+    obs <- get_obs(object, var)
+
+    dat <- lapply(seq_along(rep_list), function(m) {
+      sapply(1:nrow(rep_list[[m]]), function(k) {
+        as.data.frame(ks.test(obs, rep_list[[m]][k, ], exact = exact)[c("statistic", "p.value")])
+      }, simplify = FALSE)
+    })
+
+    return(melt_list(dat))
+}
+
+
+get_obs <- function(object, var) {
+
+  if (var %in% names(object$Mlist$refs))
+    var_dum <- attr(object$Mlist$refs[[var]], 'dummies')
+  else
+    var_dum <- var
+
+  # get the observed values from the data_list
+  if (object$analysis_type == 'survreg' & var == colnames(object$Mlist$y)) {
+    obs <- object$data_list$ctime
+    cens <- ifelse(object$data_list$cens == 1, 'censored', 'event observed')
+  } else {
+    obs <- if (var %in% names(object$data_list)) {
+      c(object$data_list[[var]])
+    } else if (var %in% object$Mlist$trafos$var) {
+      c(object$data_list$Xtrafo[, var])
+    } else if (var %in% names(object$Mlist$refs) &
+               length(levels(object$Mlist$refs[[var]])) > 2) {
+      if (var %in% names(object$data_list$Xcat)) {
+        c(object$data_list$Xcat[, var])
+      } else {
+        c(object$data_list$Xlcat[, var])
+      }
+    } else if (var_dum %in% colnames(object$data_list$Xc)) {
+      c(object$data_list$Xc[, var_dum])
+    } else if (var_dum %in% colnames(object$data_list$Xl)) {
+      c(object$data_list$Xl[, var_dum])
+    }
+    cens <- 0
+  }
+  return(obs)
+}
+
+
+ppc_mcnemar <- function(object, var, correct = TRUE) {
+  # get the replications from the MCMC sample
+  rep_list <- object$MCMC[, grep(paste0(var, "_ppc\\["),
+                                 colnames(object$MCMC[[1]]))]
+
+  obs <- factor(get_obs(object, var))
+
+  dat <- lapply(seq_along(rep_list), function(m) {
+    sapply(1:nrow(rep_list[[m]]), function(k) {
+      as.data.frame(
+        mcnemar.test(table(obs,
+                         factor(rep_list[[m]][k, ],
+                                levels = levels(obs)),
+                         exclude = NA), correct = correct)[c("statistic", "p.value")])
+    }, simplify = FALSE)
+  })
+
+  return(melt_list(dat))
+}
+
+
+
+ppc_test <- function(object, vars = NULL, summary = FALSE) {
+
+  if (is.null(vars)) {
+    vars <- c(names(object$models), colnames(object$Mlist$y))
+  }
+
+  dat <- sapply(vars, function(var) {
+    if (var %in% names(object$Mlist$refs))
+      ppc_mcnemar(object, var)
+    else
+      ppc_ks(object, var)
+  }, simplify = FALSE)
+
+
+  plotDF <- melt_list(dat)
+
+  p <- ggplot2::ggplot(plotDF,
+                       ggplot2::aes(x = p.value, group = .data$L3, color = factor(.data$L3))) +
+    ggplot2::stat_density(geom = 'line', position = 'identity') +
+    ggplot2::facet_wrap("L3", scales = 'free')
+
+  print(p)
+
+  if (summary) {
+    s <- lapply(dat, function(x) {
+      apply(x[, c('statistic', 'p.value')], 2, summary)
+    })
+    return(s)
+  }
+}
+
+
+
+# fun1 <- function(obs, rep, mu) {
+#   sqe_obs <- (obs - mu)^2
+#   sqe_rep <- (rep - mu)^2
+#   rowMeans(sqe_obs > sqe_rep, na.rm = TRUE) - 0.5
 # }
 #
-#
+# fun2 <- function(obs, rep, mu) {
+#   qd_obs <- (obs - mu)^2
+#   qd_rep <- (rep - mu)^2
+#   mean(qd_obs > qd_rep, na.rm = TRUE) - 0.5
+# }
+
 
 ## plot binned residuals
 ##
@@ -364,46 +607,45 @@
 # }
 #
 #
-# ppc_dens <- function(object, nit) {
-#   if (var %in% names(object$Mlist$refs))
-#     var_dum <- attr(object$Mlist$refs[[var]], 'dummies')
-#   else
-#     var_dum <- var
-#
+ppc_dens <- function(object, nit, var) {
+  if (var %in% names(object$Mlist$refs))
+    var_dum <- attr(object$Mlist$refs[[var]], 'dummies')
+  else
+    var_dum <- var
 
-#   MCMC <- do.call(rbind, object$MCMC)
-#   it <- sample(1:nrow(MCMC), size = nit)
-#
-#   var_rep <- MCMC[it, grep(paste0(var, "_ppc\\["), colnames(MCMC))]
-#
-#   obs <- if (var_dum %in% names(object$data_list)) {
-#     object$data_list[[var_dum]]
-#   } else if (var_dum %in% colnames(object$data_list$Xc)) {
-#     c(object$data_list$Xc[, var_dum])
-#   }
-#
-#   a <- data.frame(iter = 'obs',
-#                   value = obs, stringsAsFactors = FALSE)
-#
-#   b <- reshape2::melt(as.data.frame(cbind(iter = 1:nrow(var_rep),
-#                                           var_rep), stringsAsFactors = FALSE),
-#                       id.vars = 'iter',
-#                       variable.name = 'id')
-#   plotDF <- rbind(a, b[, names(b) != 'id'])
-#
-#   ggplot2::ggplot(plotDF, ggplot2::aes(x = value, group = iter,
-#                                        size = ifelse(iter == 'obs', 'observed', 'replicated'),
-#                                        alpha = ifelse(iter == 'obs', 'observed', 'replicated'))) +
-#     ggplot2::geom_line(stat = 'density', position = 'identity') +
-#     ggplot2::scale_size_manual(name = '',
-#                                limits = c('observed', 'replicated'),
-#                                values = c(1, 0.25)) +
-#     ggplot2::scale_alpha_manual(name = '',
-#                                 limits = c('observed', 'replicated'),
-#                                 values = c(1, 0.3))
-#
-#
-# }
+  MCMC <- do.call(rbind, object$MCMC)
+  it <- sample(1:nrow(MCMC), size = nit)
+
+  var_rep <- MCMC[it, grep(paste0(var, "_ppc\\["), colnames(MCMC))]
+
+  obs <- if (var_dum %in% names(object$data_list)) {
+    object$data_list[[var_dum]]
+  } else if (var_dum %in% colnames(object$data_list$Xc)) {
+    c(object$data_list$Xc[, var_dum])
+  }
+
+  a <- data.frame(iter = 'obs',
+                  value = obs, stringsAsFactors = FALSE)
+
+  b <- reshape2::melt(as.data.frame(cbind(iter = 1:nrow(var_rep),
+                                          var_rep), stringsAsFactors = FALSE),
+                      id.vars = 'iter',
+                      variable.name = 'id')
+  plotDF <- rbind(a, b[, names(b) != 'id'])
+
+  ggplot2::ggplot(plotDF, ggplot2::aes(x = value, group = iter,
+                                       size = ifelse(iter == 'obs', 'observed', 'replicated'),
+                                       alpha = ifelse(iter == 'obs', 'observed', 'replicated'))) +
+    ggplot2::geom_line(stat = 'density', position = 'identity') +
+    ggplot2::scale_size_manual(name = '',
+                               limits = c('observed', 'replicated'),
+                               values = c(1, 0.25)) +
+    ggplot2::scale_alpha_manual(name = '',
+                                limits = c('observed', 'replicated'),
+                                values = c(1, 0.3))
+
+
+}
 #
 # plot_qq <- function(object, var, nit, nbins, ...) {
 #
