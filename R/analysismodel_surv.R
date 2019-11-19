45abe64e18b3890440dc5b7271cdf930cdecf13a
N.S. Erler <n.erler@erasmusmc.nl>
2019-11-19 11:43:01 +0100

    change JM model writing syntax to handle multiple formulas, fix part that includes the longitudinal outcomes and rename the sub-parts that are used

--- R/analysismodel_surv.R
+++ R/analysismodel_surv.R
@@ -203,28 +203,51 @@ JM_model <- function(Mlist, K, imp_par_list_long, ...){
   }
 
 
-  paste_Xlgk <- if (length(Mlist$cols_main$Xl) > 0) {
-    paste0(" + \n", tab(indent),
-           paste_predictor(parnam = 'beta', parindex = 'j', matnam = 'Xlgk',
-                           parelmts = K["Xl", 1]:K["Xl", 2],
-                           cols = Mlist$cols_main$Xl, indent = indent,
-                           isgk = TRUE)
-    )
-  }
-
-
-  paste_Xil <- if (length(Mlist$cols_main$Xil) > 0) {
+  paste_Xil <- if (length(cols_main$Xil) > 0) {
     paste0(" + \n", tab(indent),
            paste_predictor(parnam = 'beta', parindex = 'j', matnam = 'Xil',
                            parelmts = K["Xil", 1]:K["Xil", 2],
-                           cols = Mlist$cols_main$Xil, indent = indent)
+                           cols = cols_main$Xil, indent = indent)
     )
   }
 
-  fit_Xlgk <- if (length(Mlist$cols_main$Xl) > 0) {
+
+
+  paste_mu_gk <- if (length(cols_main$Xl) > 0) {
+    paste0(" + \n", tab(indent + 3),
+           paste_predictor(parnam = 'beta', parindex = 'j', matnam = 'mu_gk',
+                           parelmts = K["Xl", 1]:K["Xl", 2],
+                           cols = cols_main$Xl, indent = indent + 3,
+                           isgk = TRUE, breakafter = 2)
+    )
+  }
+
+
+
+
+  # paste_mu_gk <- if (!is.null(Xl_cols)) {
+  #   paste0(" + \n", tab(indent),
+  #          paste_predictor(parnam = 'beta', parindex = 'j', matnam = 'Xl',
+  #                          parelmts = par_elmts["Xl", 1]:par_elmts["Xl", 2],
+  #                          cols = Xl_cols, indent = indent)
+  #   )
+  # }
+
+
+  fit_mu_gk <- if (length(cols_main$Xl) > 0) {
     paste(
       sapply(seq_along(imp_par_list_long), function(x) {
-      paste0(tab(6), "Xlgk[j, ", x, ", k] <- ",
+        Xlpart <- if (!is.null(imp_par_list_long[[x]]$Xl_cols)) {
+        pe <- imp_par_list_long[[x]]$par_elmts
+        paste0(" +\n", tab(indent + 3),
+               paste_predictor(parnam = imp_par_list_long[[x]]$parname,
+                               parindex = 'j', matnam = 'Xlgk',
+                               parelmts = pe["Xl", 1]:pe["Xl", 2], isgk = TRUE,
+                               cols = imp_par_list_long[[x]]$Xl_cols,
+                               indent = indent + 3, breakafter = 1)
+        )}
+
+      paste0(tab(6), "mu_gk[j, ", x, ", k] <- ",
              paste_ranef_predictor_gk(parnam = paste0("b_", imp_par_list_long[[x]]$varname),
                                       parindex1 = '15 * (j - 1) + k',
                                       parindex2 = 'j',
@@ -237,13 +260,39 @@ JM_model <- function(Mlist, K, imp_par_list_long, ...){
 
 
 
+  fit_Xlgk <- if (length(cols_main$Xl) > 0) {
+    paste(
+      sapply(seq_along(imp_par_list_long), function(x) {
+
+        distr <- switch(imp_par_list_long[[x]]$impmeth,
+                        "lmm" = function(varname) {
+                          paste0("dnorm(mu_gk[j, ", x, ", k], tau_", varname, ")")
+                        },
+                        "glmm_lognorm" = function(varname) {
+                          paste0("dlnorm(mu_gk[j, ", x, ", k], tau_", varname, ")")
+                        }
+                        # "binomial" =  function(varname) {
+                        #   paste0("dbern(mu_", varname, "[j])")
+                        # },
+                        # "Gamma" =  function(varname) {
+                        #   paste0("dgamma(shape_", varname, "[j], rate_", varname, "[j])")
+                        # },
+                        # "poisson" = function(varname) {
+                        #   paste0("dpois(mu_", varname, "[j])")
+                        # }
+        )
+
+        paste0(tab(6), "Xlgk[j, ", x, ", k] ~ ", distr(imp_par_list_long[[x]]$varname))
+      }), collapse = "\n")
+  }
+
   paste0(tab(4), "# Cox PH model for ", y_name, "\n",
          tab(4), "etaBaseline[j] <- ",
          if (any(is.na(K["Xc", ]))) '0'
          else
            paste_predictor(parnam = 'beta', parindex = 'j', matnam = 'Xc',
                            parelmts = K["Xc", 1]:K["Xc", 2],
-                           cols = Mlist$cols_main$Xc, indent = indent),
+                           cols = cols_main$Xc, indent = indent),
          paste_Xic, "\n",
          tab(4), "log.h0.T[j] <- inprod(Bs.gammas[], Bmat_h0[j, ])", "\n",
          tab(4), "log.hazard[j] <- log.h0.T[j] + etaBaseline[j]",
@@ -251,7 +300,9 @@ JM_model <- function(Mlist, K, imp_par_list_long, ...){
          tab(4), "for (k in 1:15) {", "\n",
          tab(6), "log.h0.s[j, k] <- inprod(Bs.gammas[], Bmat_h0s[15 * (j - 1) + k, ])", "\n",
          tab(6), "SurvLong[j, k] <- gkw[k] * exp(log.h0.s[j, k]",
-         paste_Xlgk, ")\n",
+         paste_mu_gk,
+         ")\n\n",
+         fit_mu_gk, "\n\n",
          fit_Xlgk, "\n",
          tab(4), "}", "\n\n",
          tab(4), "log.survival[j] <- -exp(etaBaseline[j]) * ", y_name ,"[j]/2 * sum(SurvLong[j, ])", "\n",
@@ -261,6 +312,9 @@ JM_model <- function(Mlist, K, imp_par_list_long, ...){
 }
 
 JM_priors <- function(K, Mlist, ...){
+  K <- K[[which(sapply(Mlist$outnam, 'attr', 'type') == 'survival')]]
+
+
   if (Mlist$ridge) {
     distr <- paste0(tab(4), "beta[k] ~ dnorm(mu_reg_surv, tau_reg_surv_ridge[k])", "\n",
                     tab(4), "tau_reg_surv_ridge[k] ~ dgamma(0.01, 0.01)", "\n")
