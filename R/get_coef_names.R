90bb1070e0dc4aa7a15c67430e7b9c58e7546082
N.S. Erler <n.erler@erasmusmc.nl>
2019-11-19 11:45:18 +0100

    changes in get_coef_names to handle multiple model formulas

--- R/get_coef_names.R
+++ R/get_coef_names.R
@@ -1,35 +1,39 @@
 
 get_coef_names <- function(Mlist, K) {
 
+  coeflist <- sapply(names(K), function(i) {
   coefs <- rbind(
-    if (length(Mlist$cols_main$Xc) > 0)
-      cbind(paste0("beta[", K["Xc", 1]:K["Xc", 2], "]"),
-            colnames(Mlist$Xc)[Mlist$cols_main$Xc]),
-    if (length(Mlist$cols_main$Xic) > 0)
+    if (length(Mlist$cols_main[[i]]$Xc) > 0)
+      cbind(paste0("beta[", K[[i]]["Xc", 1]:K[[i]]["Xc", 2], "]"),
+            colnames(Mlist$Xc)[Mlist$cols_main[[i]]$Xc]),
+    if (length(Mlist$cols_main[[i]]$Xic) > 0)
       cbind(paste0("beta[", K["Xic", 1]:K["Xic", 2], "]"),
-            colnames(Mlist$Xic)[Mlist$cols_main$Xic]),
+            colnames(Mlist$Xic)[Mlist$cols_main[[i]]$Xic]),
     if (!is.null(Mlist$hc_list))
       cbind(
         unlist(
-          sapply(names(Mlist$hc_list)[rowSums(is.na(K[names(Mlist$hc_list), , drop = FALSE])) == 0],
+          sapply(names(Mlist$hc_list)[rowSums(is.na(K[[i]][names(Mlist$hc_list)[names(Mlist$hc_list %in% rownames(K[[i]]))], , drop = FALSE])) == 0],
                  function(x) {
-                   paste0("beta[", K[x, 1]:K[x, 2], "]")
+                   paste0("beta[", K[[i]][x, 1]:K[[i]][x, 2], "]")
                  }, simplify = FALSE)
         ),
-        unlist(lapply(Mlist$hc_list[rowSums(is.na(K[names(Mlist$hc_list), , drop = FALSE])) == 0], function(x) {
+        unlist(lapply(Mlist$hc_list[rowSums(is.na(K[[i]][names(Mlist$hc_list)[names(Mlist$hc_list %in% rownames(K[[i]]))], , drop = FALSE])) == 0], function(x) {
           names(x)[sapply(x, attr, 'matrix') %in% c('Xc', 'Z')]
         }))
       ),
-    if (length(Mlist$cols_main$Xl) > 0)
-      cbind(paste0("beta[", K["Xl", 1]:K["Xl", 2], "]"),
-            colnames(Mlist$Xl)[Mlist$cols_main$Xl]),
-    if (length(Mlist$cols_main$Xil) > 0)
-      cbind(paste0("beta[", K["Xil", 1]:K["Xil", 2], "]"),
-            colnames(Mlist$Xil)[Mlist$cols_main$Xil])
+    if (length(Mlist$cols_main[[i]]$Xl) > 0)
+      cbind(paste0("beta[", K[[i]]["Xl", 1]:K[[i]]["Xl", 2], "]"),
+            colnames(Mlist$Xl)[Mlist$cols_main[[i]]$Xl]),
+    if (length(Mlist$cols_main[[i]]$Xil) > 0)
+      cbind(paste0("beta[", K[[i]]["Xil", 1]:K[[i]]["Xil", 2], "]"),
+            colnames(Mlist$Xil)[Mlist$cols_main[[i]]$Xil])
   )
-  if (max(c(0, K), na.rm = TRUE) == 1) { # case with only intercept
+  if (max(c(0, K[[i]]), na.rm = TRUE) == 1) { # case with only intercept
     coefs[, 1] <- gsub('beta\\[1\\]', 'beta', coefs[, 1])
   }
+  colnames(coefs) <- c('JAGS', 'data')
+  as.data.frame(coefs, stringsAsFactors = FALSE)
+  }, simplify = FALSE)
 
-  return(coefs)
+  if (length(coeflist) == 1) coeflist[[1]] else coeflist
 }
