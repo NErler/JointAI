ae9aa32bc0cda8f458d3caf12dbe2308f9a069ba
Nicole Erler <n.erler@erasmusmc.nl>
2019-12-29 16:11:02 +0100

    renamed file

--- R/get_modeltypes.R
+++ R/get_modeltypes.R
@@ -66,7 +66,8 @@ get_models <- function(fixed, random = NULL, data, auxvars = NULL,
 
   if (!is.null(no_model) && any(colSums(is.na(data[, no_model, drop = FALSE])) > 0)) {
     stop(gettextf("Variable(s) %s have missing values and imputation models are needed for these variables." ,
-                  paste(dQuote(no_model[colSums(is.na(data[, no_model, drop = FALSE])) > 0]), collapse = ", "),
+                  paste(dQuote(no_model[colSums(is.na(data[, no_model, drop = FALSE])) > 0]),
+                        collapse = ", "),
                   call. = FALSE)
     )
   }
@@ -109,8 +110,8 @@ get_models <- function(fixed, random = NULL, data, auxvars = NULL,
     varinfo$type[!varinfo$tvar & varinfo$nlev > 2 & varinfo$ordered] <- 'clm'
     varinfo$type[varinfo$tvar & varinfo$nlev > 2 & !varinfo$ordered] <- 'mlogitmm'
     varinfo$type[!varinfo$tvar & varinfo$nlev > 2 & !varinfo$ordered] <- 'mlogit'
-    varinfo$type[varinfo$tvar & varinfo$nlev == 2] <- 'glmm_logit'
-    varinfo$type[!varinfo$tvar & varinfo$nlev == 2] <- 'glm_logit'
+    varinfo$type[varinfo$tvar & varinfo$nlev == 2] <- 'glmm_binomial_logit'
+    varinfo$type[!varinfo$tvar & varinfo$nlev == 2] <- 'glm_binomial_logit'
     varinfo$type[varinfo$tvar & varinfo$nlev == 0] <- 'lmm'
     varinfo$type[!varinfo$tvar & varinfo$nlev == 0] <- 'lm'
     varinfo$type[varinfo$out] <- sapply(fixed, attr, 'type')
@@ -127,7 +128,7 @@ get_models <- function(fixed, random = NULL, data, auxvars = NULL,
     # if (all(nmis == 0) & is.null(models_user) & (is.null(analysis_type) || analysis_type!= "JM"))
     #   return(list(models = NULL, meth = NULL))
 
-    varinfo <- subset(varinfo, !L1 %in% no_model)
+    varinfo <- varinfo[which(!varinfo$L1 %in% no_model), , drop = FALSE]
 
     types <- split(varinfo,
                    ifelse(varinfo$out, 'outcome',
@@ -169,36 +170,8 @@ get_models <- function(fixed, random = NULL, data, auxvars = NULL,
 
     models <- unlist(setNames(models$type, models$L1))
 
-    # models <- c(
-    #   types$incomplete.tvar,
-    #   if (!is.null(types$incomplete.baseline) | (!is.null(analysis_type) && analysis_type == 'JM')) {
-    #     types$complete.tvar
-    #   } else {
-    #     if (any(names(models) %in% names(types$complete.tvar)))
-    #       types$complete.tvar[names(types$complete.tvar) %in% names(models)]
-    #   },
-    #   types$incomplete.baseline,
-    #   if (any(names(models) %in% names(types$complete.baseline)))
-    #     types$complete.baseline[names(types$complete.baseline) %in% names(models)]
-    # )
-    #
-    # nlevel <- sapply(data[, names(models), drop = FALSE],
-    #                  function(x) length(levels(x)))
-
-    # if (length(nlevel) > 0) {
-    #   models[nlevel == 0 & !tvar[names(nlevel)]] <- "norm"
-    #   models[nlevel == 0 &  tvar[names(nlevel)]] <- "lmm"
-    #   models[nlevel == 2 & !tvar[names(nlevel)]] <- "logit"
-    #   models[nlevel == 2 &  tvar[names(nlevel)]] <- "glmm_logit"
-    #   models[nlevel  > 2 & !tvar[names(nlevel)]] <- "multilogit"
-    #   models[nlevel  > 2 &  tvar[names(nlevel)]] <- "mlmm"
-    #   models[sapply(data[, names(nlevel), drop = FALSE], is.ordered) & !tvar[names(nlevel)]] <- "cumlogit"
-    #   models[sapply(data[, names(nlevel), drop = FALSE], is.ordered) & tvar[names(nlevel)]] <- "clmm"
-    # }
 
     models[names(models_user)] <- models_user
-
-    # meth <- if(any(nmis[names(models)] > 0)) models[nmis[names(models)] > 0]
 
     if (any(models %in% c('mlogitmm'))) {
       stop(paste0("JointAI can't yet handle unordered longitudinal categorical covariates (>2 levels).\n",
@@ -206,17 +179,6 @@ get_models <- function(fixed, random = NULL, data, auxvars = NULL,
     }
   } else {
     models <- NULL
-    # meth <- NULL
   }
   return(models)
 }
-
-
-
-# #' @rdname get_models
-# #' @export
-# get_imp_meth <- function(fixed, random = NULL, data,
-#                          auxvars = NULL, no_model = NULL, models = NULL){
-#   get_models(fixed = fixed, random = random, data = data, auxvars = auxvars,
-#              no_model = no_model, models = models)$meth
-# }
