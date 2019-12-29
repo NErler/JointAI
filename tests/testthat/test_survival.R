da69adb209277ea0301c98b76e48df00e91aed83
Nicole Erler <n.erler@erasmusmc.nl>
2019-12-29 16:23:57 +0100

    use I() when usin a function to specify event factor in a survival model

--- tests/testthat/test_survival.R
+++ tests/testthat/test_survival.R
@@ -6,7 +6,7 @@ test_that('survreg models work', {
   expect_s3_class(survreg_imp(Surv(time, status) ~ 1, data = lung, n.iter = 10),
                   class = 'JointAI')
 
-  expect_s3_class(survreg_imp(Surv(stop, status > 0) ~ 1, data = bladder1[bladder1$stop > 0, ], n.iter = 10),
+  expect_s3_class(survreg_imp(Surv(stop, I(status > 0)) ~ 1, data = bladder1[bladder1$stop > 0, ], n.iter = 10),
                   class = 'JointAI')
 
   expect_s3_class(survreg_imp(Surv(time, status) ~ wt.loss + meal.cal + ph.ecog, data = lung, n.iter = 10),
@@ -22,7 +22,8 @@ test_that('coxph models work', {
   expect_s3_class(coxph_imp(Surv(time, status) ~ 1, data = lung, n.iter = 10),
                   class = 'JointAI')
 
-  expect_s3_class(coxph_imp(Surv(stop, status > 0) ~ 1, data = bladder1[bladder1$stop > 0, ], n.iter = 10),
+  expect_s3_class(coxph_imp(Surv(stop, I(status > 0)) ~ 1,
+                            data = bladder1[bladder1$stop > 0, ], n.iter = 10),
                   class = 'JointAI')
 
   expect_s3_class(coxph_imp(Surv(time, status) ~ wt.loss + meal.cal + ph.ecog, data = lung, n.iter = 10),
