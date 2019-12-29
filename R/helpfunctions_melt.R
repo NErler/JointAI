5c6e399084359f140b96a6b2eebb6640bf6e4dc2
Nicole Erler <n.erler@erasmusmc.nl>
2019-12-29 16:13:43 +0100

    functions for  "melting" moved to separate file

---
+++ R/helpfunctions_melt.R
@@ -0,0 +1,105 @@
+
+melt_list <- function(l, varname = "L1", valname = NULL) {
+
+  do.call(rbind,
+          lapply(seq_along(l), function(k) {
+            if (is.vector(l[[k]]) & !is.null(valname))
+              df <- as.data.frame(list(l[[k]]), col.names = valname,
+                                  stringsAsFactors = FALSE)
+            else
+              df <- as.data.frame(l[[k]], stringsAsFactors = FALSE)
+
+            df[, varname] <- names(l)[k]
+            df
+          }))
+}
+
+
+melt_matrix <- function(X, varnames = NULL, valname = 'value') {
+  if (!inherits(X, 'matrix'))
+    stop("This function may not work for objects that are not matrices.")
+
+  dimnam <- if (is.null(varnames)) {
+    if (is.null(names(dimnames(X)))) {
+      paste0('V', 1:length(dim(X)))
+    } else {
+      names(dimnames(X))
+    }
+  } else {varnames}
+
+  g <- lapply(seq_along(dimnam), function(k) {
+    if (is.null(dimnames(X)[[k]]))
+      seq_len(dim(X)[k])
+    else dimnames(X)[[k]]
+  })
+  names(g) <- dimnam
+
+  out <- expand.grid(g, stringsAsFactors = FALSE)
+  out[, valname] <- c(X)
+
+  attr(out, 'out.attrs') <- NULL
+  return(out)
+}
+
+melt_matrix_list <- function(X, varnames = NULL) {
+  if (!inherits(X, 'list') || !all(sapply(X, inherits, 'matrix')))
+    stop("This function may not work for objects that are not a list of matrices.")
+
+  Xnew <- lapply(X, melt_matrix, varnames = varnames)
+  Xnew <- lapply(seq_along(Xnew), function(k) {
+    cbind(Xnew[[k]], L1 = k)
+  })
+
+  out <- do.call(rbind, Xnew)
+
+  attr(out, 'out.attrs') <- NULL
+  return(out)
+}
+
+
+melt_data.frame <- function(data, id.vars = NULL, varnames = NULL, valname = 'value') {
+  if (!inherits(data, 'data.frame'))
+    stop("This function may not work for objects that are not data.frames.")
+
+  data$rowID <- paste0('rowID', 1:nrow(data))
+  X <- data[, !names(data) %in% c('rowID', id.vars), drop = FALSE]
+
+  g <- list(rowID = data$rowID,
+            variable = if (ncol(X) > 0) names(X)
+  )
+
+  out <- expand.grid(Filter(Negate(is.null), g), stringsAsFactors = FALSE)
+
+  if (length(unique(sapply(X, class))) > 1) {
+    out[, valname] <- unlist(lapply(X, as.character))
+  } else {
+    out[, valname] <- unlist(X)
+  }
+
+  mout <- merge(data[, c("rowID", id.vars)], out)
+
+  attr(mout, 'out.attrs') <- NULL
+
+  if (ncol(X) > 0) mout[order(mout$variable), -1] else mout
+}
+
+
+melt_data.frame_list <- function(X, id.vars = NULL, varnames = NULL, valname = 'value') {
+  if (!inherits(X, 'list') || !all(sapply(X, inherits, 'data.frame') | sapply(X, inherits, 'NULL')))
+    stop("This function may not work for objects that are not a list of data frames.")
+
+  Xnew <- lapply(X[!sapply(X, is.null)],
+                 melt_data.frame, varnames = varnames, id.vars = id.vars)
+
+  if (is.null(names(Xnew)))
+    names(Xnew) <- seq_along(Xnew)
+
+  Xnew <- lapply(names(Xnew), function(k) {
+    cbind(Xnew[[k]], L1 = k, stringsAsFactors = FALSE)
+  })
+
+  out <- do.call(rbind, Xnew)
+
+  attr(out, 'out.attrs') <- NULL
+  return(out)
+}
