6bb66883948acc0f59709b7e4d0fcaa8209e9934
Nicole Erler <n.erler@erasmusmc.nl>
2020-02-06 08:13:19 +0100

    comment out un-used residuals syntax

--- R/methods.R
+++ R/methods.R
@@ -40,57 +40,62 @@ family.JointAI <- function(object, ...) {
 }
 
 
-#' Extract residuals from an object of class JointAI
-#'
-#' @inheritParams sharedParams
-#' @param type type of residuals: \code{"deviance"}, \code{"response"},
-#'             \code{"working"}
-#' @param ... currently not used
-#'
-#' @section Note:
-#' \itemize{
-#' \item For mixed models residuals are currently calculated using the fixed effects only.
-#' \item For ordinal (mixed) models and parametric survival models only \code{type = "response"} is available.
-#' \item For Cox proportional hazards models residuals are not yet implemented.
-#' }
-#'
-#' @examples
-#' mod <- glm_imp(B1 ~ C1 + C2 + O1, data = wideDF, n.iter = 100,
-#'                family = binomial(), mess = FALSE)
-#' summary(residuals(mod, type = 'response'))
-#' summary(residuals(mod, type = 'working'))
-#'
-#'
-#' @export
-residuals.JointAI <- function(object,
-                              type = c('deviance', 'response', 'working'), ...) {
-  type <- match.arg(type)
-
-  if (object$analysis_type %in% c('survreg') & type %in% c('working', 'deviance'))
-    stop(gettextf("Residuals of type %s are not implemented for a JointAI model of type %s. Currently only residuals of type %s are available for parametric survival models.",
-                  dQuote(type), dQuote(object$analysis_type), dQuote('response')),
-         call. = FALSE)
-
-  if (object$analysis_type %in% c('coxph', 'clm', 'clmm', 'JM'))
-    stop(gettextf("Residuals are not yet implemented for a JointAI model of type %s.",
-                  dQuote(object$analysis_type)), call. = FALSE)
-
-  # r <- object$residuals
-  y <- object$data_list[[names(object$Mlist$y)]]
-  mu <- object$fitted.values
-  wts <- rep(1, length(mu))
-
-  if (object$analysis_type == 'survreg') {
-    MCMC <- prep_MCMC(object, ...)
-  }
-
-
-
-  res <- switch(type,
-                working = (y - mu)/family(object)$mu.eta(predict(object, type = 'link')$fit),
-                response = y - mu,
-                deviance = sqrt(pmax((family(object)$dev.resids)(y, mu, wts), 0)) * ifelse(y > mu, 1, -1)
-  )
-  return(res)
-}
+#
+# residuals.JointAI <- function(object,
+#                               type = c('deviance', 'response', 'working'), ...) {
+#   type <- match.arg(type)
+#
+#   if (object$analysis_type %in% c('survreg') & type %in% c('working', 'deviance'))
+#     stop(gettextf("Residuals of type %s are not implemented for a JointAI model of type %s. Currently only residuals of type %s are available for parametric survival models.",
+#                   dQuote(type), dQuote(object$analysis_type), dQuote('response')),
+#          call. = FALSE)
+#
+#   if (object$analysis_type %in% c('coxph', 'clm', 'clmm', 'JM'))
+#     stop(gettextf("Residuals are not yet implemented for a JointAI model of type %s.",
+#                   dQuote(object$analysis_type)), call. = FALSE)
+#
+#   # r <- object$residuals
+#   y <- object$data_list[[names(object$Mlist$y)]]
+#   mu <- object$fitted.values
+#   wts <- rep(1, length(mu))
+#
+#   if (object$analysis_type == 'survreg') {
+#     MCMC <- prep_MCMC(object, ...)
+#   }
+#
+#   res <- switch(type,
+#                 working = (y - mu)/family(object)$mu.eta(predict(object, type = 'link')$fit),
+#                 response = y - mu,
+#                 deviance = sqrt(pmax((family(object)$dev.resids)(y, mu, wts), 0)) * ifelse(y > mu, 1, -1)
+#   )
+#   return(res)
+# }
+#
+#
+# residuals1 <- function(modeltype, type, y, fitted.values) {
+#
+#   if (modeltype %in% c('survreg') & type %in% c('working', 'deviance'))
+#     stop(gettextf("Residuals of type %s are not implemented for a JointAI model of type %s. Currently only residuals of type %s are available for parametric survival models.",
+#                   dQuote(type), dQuote(modeltype), dQuote('response')),
+#          call. = FALSE)
+#
+#   if (modeltype %in% c('coxph', 'clm', 'clmm', 'JM'))
+#     stop(gettextf("Residuals are not yet implemented for a JointAI model of type %s.",
+#                   dQuote(modeltype)), call. = FALSE)
+#
+#   # y <- object$data_list[[names(object$Mlist$y)]]
+#   mu <- fitted.values
+#   wts <- rep(1, length(mu))
+#
+#   # if (modeltype == 'survreg') {
+#   #   MCMC <- prep_MCMC(object, ...)
+#   # }
+#
+#   res <- switch(type,
+#                 working = (y - mu)/family$mu.eta(predict(object, type = 'link')$fit),
+#                 response = y - mu,
+#                 deviance = sqrt(pmax((family$dev.resids)(y, mu, wts), 0)) * ifelse(y > mu, 1, -1)
+#   )
+#   return(res)
+# }
 
