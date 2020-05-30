9e67f70335b052167fd357671885c234df13843d
Nicole Erler <n.erler@erasmusmc.nl>
2020-05-30 11:14:03 +0200

    add pbc data and move simlong documentation to other file

--- R/datasets.R
+++ R/datasets.R
@@ -73,3 +73,166 @@
 #'
 #'
 "wideDF"
+
+
+
+
+
+#' Simulated Longitudinal Data in Long and Wide Format
+#'
+#' This data was simulated to mimic data from a longitudinal cohort study
+#' following mothers and their child from birth until approximately 4 years of
+#' age.
+#' It contains 2400 observations of 200 mother-child pairs. Children's BMI and
+#' head circumference was measured repeatedly and their age in months was recorded
+#' at each measurement. Furthermore, the data contain several baseline variables
+#' with information on the mothers' demographics and socioeconomic status.
+#'
+#'
+#' @format
+#' \code{simLong}: A data frame in long format with 2400 rows and 16 variables
+#'
+#' \code{simWide}: A data frame in wide format with 200 rows and 81 variables
+#'
+#' @section Baseline covariates: (in \code{simLong} and \code{simWide})
+#' \describe{
+#'   \item{GESTBIR}{gestational age at birth (in weeks)}
+#'   \item{ETHN}{ethnicity (binary: European vs. other)}
+#'   \item{AGE_M}{age of the mother at intake}
+#'   \item{HEIGHT_M}{height of the mother (in cm)}
+#'   \item{PARITY}{number of times the mother has given birth (binary: 0 vs. >=1)}
+#'   \item{SMOKE}{smoking status of the mother during pregnancy
+#'                (3 ordered categories: never smoked during pregnancy,
+#'                 smoked until pregnancy was known, continued smoking in pregnancy)}
+#'   \item{EDUC}{educational level of the mother (3 ordered categories: low, mid, high)}
+#'   \item{MARITAL}{marital status (3 categories)}
+#'   \item{ID}{subject identifier}
+#'   }
+#'
+#'
+#'
+#' @section Long-format variables: (only in \code{simLong})
+#' \describe{
+#'    \item{time}{measurement occasion/visit
+#'               (by design, children should be measured at/around
+#'               1, 2, 3, 4, 7, 11, 15, 20, 26, 32, 40 and 50 months of age)}
+#'   \item{age}{child age at measurement time in months}
+#'   \item{bmi}{child BMI}
+#'   \item{hc}{child head circumference in cm}
+#'   \item{hgt}{child height in cm}
+#'   \item{wgt}{child weight in gram}
+#'   \item{sleep}{sleeping behavior of the child (3 ordered categories)}
+#' }
+#'
+#'
+#' @section Wide-format variables: (only in \code{simWide})
+#' \describe{
+#'   \item{age1, age2, age3, age4, age7, age11, age15, age20, age26, age32,
+#'    age40, age50}{child age at the repeated measurements in months}
+#'   \item{bmi1, bmi2, bmi3, bmi4, bmi7, bmi11, bmi15, bmi20, bmi26, bmi32,
+#'    bmi40, bmi50}{repeated measurements of child BMI}
+#'   \item{hc1, hc2, hc3, hc4, hc7, hc11, hc15, hc20, hc26, hc32,
+#'    hc40, hc50}{repeated measurements of child head circumference in cm}
+#'   \item{hgt1, hgt2, hgt3, hgt4, hgt7, hgt11, hgt15, hgt20, hgt26, hgt32,
+#'    hgt40, hgt50}{repeated measurements of child height in cm}
+#'   \item{wgt1, wgt2, wgt3, wgt4, wgt7, wgt11, wgt15, wgt20, wgt26, wgt32,
+#'    wgt40, wgt50}{repeated measurements of child weight in gram}
+#'   \item{sleep1, sleep2, sleep3, sleep4, sleep7, sleep11, sleep15, sleep20,
+#'    sleep26, sleep32, sleep40, sleep50}{repeated measurements of child sleep
+#'    behaviour (3 ordered categories)}
+#' }
+#'
+#'
+#'
+#' @keywords datasets
+#' @docType data
+#' @examples
+#'  summary(simLong)
+#'  summary(simWide)
+#'
+#' @name simLong
+NULL
+
+#' @rdname simLong
+"simLong"
+
+#' @rdname simLong
+"simWide"
+
+
+
+
+#' PBC data
+#'
+#' Data from the Mayo Clinic trial in primary biliary cirrhosis (PBC) of the
+#' liver. This dataset was obtained from the \strong{survival} package:
+#' the variables \code{copper} and \code{trig} from \code{survival::pbc} were
+#' merged into \code{survival::pbcseq} and several categorical variables were
+#' re-coded.
+#'
+#'
+#' @format
+#' \code{pbc}: A data frame of 312 individuals in long format with 1945 rows
+#'             and 21 variables.
+#'
+#' @section Survival outcome and id:
+#' \describe{
+#'   \item{id}{case number}
+#'   \item{futime}{number of days between registration and the earlier of death,
+#'                 transplantation, or end of follow-up}
+#'   \item{status}{status at endpoint, 0/1/2 for censored, transplant, dead}
+#'   }
+#'
+#'
+#' @section Baseline covariates:
+#' \describe{
+#'   \item{trt}{D-pen (D-penicillmain) vs  placebo}
+#'   \item{age}{in years}
+#'   \item{sex}{male or female}
+#'   \item{copper}{urine copper (ug/day)}
+#'   \item{trig}{triglycerides (mg/dl)}
+#'   }
+#'
+#'
+#' @section Time-varying covariates:
+#' \describe{
+#'   \item{day}{number of days between enrollment and this visit date all measurements below refer to this date}
+#'   \item{albumin}{serum albumin (mg/dl)}
+#'   \item{alk.phos}{alkaline phosphotase (U/liter)}
+#'   \item{ascites}{presence of ascites}
+#'   \item{ast}{aspartate aminotransferase, once called SGOT (U/ml)}
+#'   \item{bili}{serum bilirunbin (mg/dl)}
+#'   \item{chol}{serum cholesterol (mg/dl)}
+#'   \item{edema}{0 no edema, 0.5 untreated or successfully treated 1 edema despite diuretic therapy}
+#'   \item{hepato}{presence of hepatomegaly or enlarged liver}
+#'   \item{platelet}{platelet count}
+#'   \item{protime}{standardised blood clotting time}
+#'   \item{spiders}{blood vessel malformations in the skin}
+#'   \item{stage}{histologic stage of disease (needs biopsy)}
+#'   }
+#'
+#'
+#' @keywords datasets
+#' @docType data
+#' @examples
+#'  summary(pbc)
+#'
+#' @name pbc
+NULL
+
+
+# pbc <- merge(survival::pbcseq,
+#              subset(survival::pbc, select = c(id, copper, trig))
+# )
+# pbc$trt <- factor(pbc$trt, levels = 0:1, labels = c('D-pen', 'placebo'))
+# pbc$ascites <- factor(pbc$ascites, levels = 0:1, labels = c('no', 'yes'))
+# pbc$hepato <- factor(pbc$hepato, levels = 0:1, labels = c('no', 'yes'))
+# pbc$spiders <- factor(pbc$spiders, levels = 0:1, labels = c('no', 'yes'))
+# pbc$edema <- factor(pbc$edema, levels = c(0, 0.5, 1),
+#                     labels = c('no', '(un)treated', 'edema'))
+# pbc$stage <- factor(pbc$stage, levels = 1:4, labels = 1:4, ordered = TRUE)
+# pbc$sex <- factor(pbc$sex, levels = c('m','f'), labels = c('male', 'female'))
+# pbc$status <- factor(pbc$status, levels = 0:2,
+#                      labels = c('censored', 'transplant', 'dead'))
+#
+# save(pbc, file = 'data/pbc.RData')
