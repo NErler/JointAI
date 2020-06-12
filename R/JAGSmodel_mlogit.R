92bb91f367adc88fe03c596820111f996a95acfe
Nicole Erler <n.erler@erasmusmc.nl>
2020-06-12 12:39:43 +0200

    clean-up mlogit JAGSmodel code (re-arrange syntax, add comments, add empty lines around dummies)

--- R/JAGSmodel_mlogit.R
+++ R/JAGSmodel_mlogit.R
@@ -1,26 +1,14 @@
 
 JAGSmodel_mlogit <- function(info) {
+
+  # specify indent width and index character to be used
   index <- info$index[gsub("M_", "", info$resp_mat)]
   indent <- nchar(info$varname) + 23
 
-  probs <- sapply(1:info$ncat, function(k){
-    paste0(tab(4), "p_", info$varname, "[", index, ", ", k,
-           "] <- min(1-1e-7, max(1e-7, phi_", info$varname, "[", index, ", ", k,
-           "] / sum(phi_", info$varname, "[", index, ", ])))")
-    })
+  # main model elements --------------------------------------------------------
 
-
-
-  # Mc_predictor <- mapply(function(k, par_elmts) {
-  #   paste0(tab(4), "log(phi_", info$varname, "[", index, ", ", k, "]) <- ",
-  #          paste_predictor(parnam = info$parname, parindex = index,
-  #                          matnam = 'Mc', parelmts = par_elmts, cols = info$lp$Mc,
-  #                          scale_pars = info$scale_pars$Mc, indent = indent)
-  #   )
-  # }, k = 2:info$ncat, info$parelmts$Mc)
-
-
-  Mc_predictor <- mapply(function(k, par_elmts) {
+  # linear predictor of baseline covariates (including interaction terms)
+  linpred <- mapply(function(k, par_elmts) {
     paste0(tab(4), "log(phi_", info$varname, "[", index, ", ", k, "]) <- ",
            add_linebreaks(
              paste_linpred(info$parname,
@@ -33,14 +21,28 @@ JAGSmodel_mlogit <- function(info) {
   }, k = 2:info$ncat, par_elmts = info$parelmts[[info$resp_mat]])
 
 
+  # syntax for probabilities, using min-max-trick for numeric stability
+  # i.e., "p_M2[ii, 1] <- min(1-1e-7, max(1e-7, phi_M2[ii, 1] / sum(phi_M2[ii, ])))"
+  probs <- sapply(1:info$ncat, function(k){
+    paste0(tab(4), "p_", info$varname, "[", index, ", ", k,
+           "] <- min(1-1e-7, max(1e-7, phi_", info$varname, "[", index, ", ", k,
+           "] / sum(phi_", info$varname, "[", index, ", ])))")
+    })
+
+
+
+  # syntax for logs, e.g., "log(phi_M2[ii, 2]) <- M_id[ii, 3] * alpha[8] + ..."
   logs <- c(paste0(tab(4), "log(phi_", info$varname, "[", index, ", 1]) <- 0"),
-            Mc_predictor)
+            linpred)
 
 
+  # syntax to set values of dummy variables,
+  # e.g. "M_lvlone[i, 8] <- ifelse(M_lvlone[i, 4] == 2, 1, 0)"
   dummies <- if (!is.null(info$dummy_cols)) {
-    paste0(c('\n', paste_dummies(categories = info$categories, dest_mat = info$resp_mat,
-                               dest_col = info$resp_col, dummy_cols = info$dummy_cols,
-                               index = index)), collapse = "\n")
+    paste0('\n\n', paste0(
+      paste_dummies(categories = info$categories, dest_mat = info$resp_mat,
+                    dest_col = info$resp_col, dummy_cols = info$dummy_cols,
+                    index = index), collapse = "\n"), "\n")
   }
 
   paste0(tab(2), add_dashes(paste0("# Multinomial logit model for ", info$varname)), "\n",
@@ -51,6 +53,8 @@ JAGSmodel_mlogit <- function(info) {
          paste0(logs, collapse = "\n"),
          dummies, "\n",
          tab(), "}", "\n\n",
+
+         # priors
          tab(), "# Priors for the model for ", info$varname,"\n",
          tab(), "for (k in ", min(unlist(info$parelmts)), ":", max(unlist(info$parelmts)), ") {", "\n",
          get_priordistr(info$shrinkage, type = 'multinomial', parname = info$parname),
