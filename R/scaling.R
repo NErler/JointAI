b3373f04611172e7efe211c0285007c134295e7c
Nicole Erler <n.erler@erasmusmc.nl>
2020-06-13 13:13:01 +0200

    remove old syntax and more rescaling function to file scaling.R

--- R/scaling.R
+++ R/scaling.R
@@ -38,3 +38,45 @@ get_scale_pars <- function(mat, groups, scale_vars) {
 
 
 
+
+# re-scale ---------------------------------------------------------------------
+rescale <- function(MCMC, coefs, scale_pars, info_list) {
+  # MCMC: a mcmc object (only one element of the mcmc.list)
+  # coefs: combined coef_list (do.call(rbind, coef_list))
+  # scale_pars: combined scaling parameters (do.call(rbind, unname(Mlist$scale_pars)))
+  # info_list
+
+  scale_pars$center[is.na(scale_pars$center)] <- 0
+  scale_pars$scale[is.na(scale_pars$scale)] <- 1
+
+  sapply(colnames(MCMC), function(k) {
+    if (k %in% coefs$coef) {
+      # variable name
+      varnam <- coefs$varname[which(coefs$coef == k)]
+
+      if (varnam == "(Intercept)") {
+        outcome <- coefs$outcome[which(coefs$coef == k)]
+        covnames <- names(unlist(unname(info_list[[outcome]]$lp)))
+        covnames <- covnames[which(!covnames %in% "(Intercept)")]
+
+        if (length(covnames) > 0) {
+          scaled_covs <- sapply(covnames, function(j) {
+            MCMC[, coefs$coef[match(j, coefs$varname)], drop = FALSE] *
+              scale_pars[j, 'center']/scale_pars[j, 'scale']
+          })
+
+          MCMC[, k] - rowSums(scaled_covs)
+        } else {
+          MCMC[, k]
+        }
+      } else {
+        # scaling parameters
+        sp <- scale_pars[varnam, ]
+
+        MCMC[, k] / sp$scale
+      }
+    } else {
+      MCMC[, k]
+    }
+  })
+}
