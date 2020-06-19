9e77aadb4f12ce055fe4248a041c76ff9bfc4e30
Nicole Erler <n.erler@erasmusmc.nl>
2020-06-19 19:41:43 +0200

    update of get_plotmain() to use the varnam_print column of coef_list instead of the varname column, so that coefficients for different linear predictors (in multinomial logit(mixed) models) can be distinguished

--- R/plots.R
+++ R/plots.R
@@ -350,15 +350,15 @@ get_plotmain <- function(object, plotnams, ylab = FALSE) {
 
   if (length(unique(coef_set$outcome)) == 1) {
     plotnams[na.omit(match(coefs$coef, plotnams))] <-
-      coefs$varname[na.omit(match(plotnams, coefs$coef))]
+      coefs$varnam_print[na.omit(match(plotnams, coefs$coef))]
   } else {
     if (ylab) {
       plotnams[na.omit(match(coefs$coef, plotnams))] <-
         paste0(coefs$outcome[sort(na.omit(match(plotnams, coefs$coef)))], ": ",
-               coefs$varname[sort(na.omit(match(plotnams, coefs$coef)))])
+               coefs$varnam_print[sort(na.omit(match(plotnams, coefs$coef)))])
     } else {
       plotnams[na.omit(match(coefs$coef, plotnams))] <-
-        paste0(coefs$varname[sort(na.omit(match(plotnams, coefs$coef)))], "\n",
+        paste0(coefs$varnam_print[sort(na.omit(match(plotnams, coefs$coef)))], "\n",
                "(", coefs$outcome[sort(na.omit(match(plotnams, coefs$coef)))], ")")
     }
   }
