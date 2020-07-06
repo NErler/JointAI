9253360b4be1de91b01a7af6df900920f7cf531d
Nicole Erler <n.erler@erasmusmc.nl>
2020-07-06 07:16:32 +0200

    take into account that covariates with non-proportional effect in ordinal models need more parameters because they appear in multiple linear predictors

--- R/get_model_dimensions.R
+++ R/get_model_dimensions.R
@@ -1,7 +1,7 @@
 
 # get the model dimension (number of parameters per design matrix for the
 # different levels) for a single model
-get_1model_dim <- function(lp_cols, modeltype, ncat) {
+get_1model_dim <- function(lp_cols, modeltype, ncat, lp_nonprop) {
 
   # prepare matrix to save parameter indices in
   K <- matrix(NA,
@@ -15,7 +15,16 @@ get_1model_dim <- function(lp_cols, modeltype, ncat) {
 
   if (length(lp_cols) > 0)
     for (i in names(lp_cols)) {
-      K[i, ] <- c(1, nlp * length(lp_cols[[i]])) + max(c(K, 0), na.rm = TRUE)
+      if (modeltype %in% c('clm', 'clmm')) {
+        # for ordinal models, the number of parameter is the number of columns
+        # given in lp_cols plus extra parameters for the non-proportional
+        # effects (which are already included in lp_cols once, therefore ncat -
+        # 2)
+        K[i, ] <- c(1, length(lp_cols[[i]]) + length(lp_nonprop[[i]]) * (ncat - 2)) +
+          max(c(K, 0), na.rm = TRUE)
+      } else {
+        K[i, ] <- c(1, nlp * length(lp_cols[[i]])) + max(c(K, 0), na.rm = TRUE)
+      }
     }
   K
 }
@@ -29,7 +38,8 @@ get_model_dim <- function(lp_cols, Mlist) {
 
     Klist <- sapply(names(lp_cols), function(i) {
       get_1model_dim(lp_cols = lp_cols[[i]], modeltype = Mlist$models[i],
-                     ncat = length(levels(Mlist$refs[[i]])))
+                     ncat = length(levels(Mlist$refs[[i]])),
+                     lp_nonprop = Mlist$lp_nonprop[[i]])
     }, simplify = FALSE)
 
     for (i in seq_along(Klist)[-1]) {
