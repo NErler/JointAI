353b0c9ccf0502485aaf26002044a149c042cc6a
Nicole Erler <n.erler@erasmusmc.nl>
2020-07-08 16:05:46 +0200

    add test for joint model

--- tests/testthat/test_survival.R
+++ tests/testthat/test_survival.R
@@ -41,3 +41,12 @@ test_that('coxph models work', {
                   class = 'JointAI')
 
 })
+
+test_that('Joint model with ordinal covariate works', {
+  test <- JM_imp(list(Surv(futime, status != 'alive') ~ stage + age + sex,
+                      stage ~ age + sex + (1 | id)),
+                 data = PBC, timevar = "day",
+                 n.adapt = 3, n.iter = 3)
+
+  expect_s3_class(test, class = "JointAI")
+})
