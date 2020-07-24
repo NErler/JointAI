ee93e1dd487e49948ed11c4dbb7cc6c55fbb0dbd
Nicole Erler <n.erler@erasmusmc.nl>
2020-07-24 16:14:53 +0200

    remove commented-out code

--- R/get_params.R
+++ R/get_params.R
@@ -110,7 +110,7 @@ get_modelpars <- function(info_list, Mlist, args, set = "main") {
   params <- NULL
 
 
-  # regcoef
+  # regression coefficients alpha & beta
   coefnam <- switch(set,
                     main = "betas",
                     other = "alphas")
@@ -122,14 +122,14 @@ get_modelpars <- function(info_list, Mlist, args, set = "main") {
     params <- c(params, gsub("s$", "", coefnam))
 
 
-  # basehaz
+  # parameters basehaz
   if (args$basehaz & any(modeltypes %in% c("coxph", "JM"))) {
     survnams <- sapply(sublist[modeltypes %in% c("coxph", "JM")],
                        "[[", "varname")
     params <- c(params, paste0("beta_Bh0_", survnams))
   }
 
-  # gamma & delta
+  # parameters gamma & delta
   if (any(modeltypes %in% c("clm", "clmm"))) {
     ord_mods <- names(sublist)[modeltypes %in% c("clm", "clmm")]
     params <- c(params,
@@ -140,7 +140,7 @@ get_modelpars <- function(info_list, Mlist, args, set = "main") {
   }
 
 
-  # sigma & tau
+  # parameters sigma & tau
   sigvars <- if (args[[paste0("sigma_", set)]]) {
     names(sublist)[families %in% c("gaussian", "Gamma", "lognorm")]
   }
@@ -229,286 +229,3 @@ get_ranefpars <- function(info_list, Mlist, args, set = "main") {
 
   params
 }
-
-
-
-
-
-
-
-
-
-# get_params_old <- function(Mlist, info_list,
-#                        analysis_main = TRUE,
-#                        analysis_random = FALSE,
-#                        other_models = FALSE,
-#                        imps = NULL,
-#                        ppc = NULL,
-#                        betas = NULL, tau_main = NULL, sigma_main = NULL,
-#                        gamma_main = NULL, delta_main = NULL,
-#                        ranef_main = NULL, invD_main = NULL,
-#                        D_main = NULL, RinvD_main = NULL,
-#                        alphas = NULL, tau_other = NULL, sigma_other = NULL,
-#                        gamma_other = NULL, delta_other = NULL,
-#                        ranef_other = NULL, invD_other = NULL,
-#                        D_other = NULL, RinvD_other = NULL,
-#                        other = NULL, mess = TRUE, basehaz = NULL, ...) {
-#
-#
-#   # split info_list into main models and other models
-#   list_main <- info_list[names(Mlist$fixed)]
-#   list_other <- info_list[!names(info_list) %in% names(Mlist$fixed)]
-#
-#   modeltypes_main <- sapply(list_main, "[[", "modeltype")
-#   modeltypes_other <- sapply(list_other, "[[", "modeltype")
-#
-#   families_main <- sapply(list_main, "[[", "family")
-#   families_other <- sapply(list_other, "[[", "family")
-#
-#
-#   # get random effects info for the main models and other models
-#   ranef_info_main <- lapply(list_main, function(x)
-#     if (!is.null(x$hc_list))
-#       list(varname = x$varname,
-#            lvls = names(x$hc_list$hcvars),
-#            nranef = x$nranef)
-#   )
-#
-#   ranef_info_other <- lapply(list_other, function(x)
-#     if (!is.null(x$hc_list))
-#       list(varname = x$varname,
-#            lvls = names(x$hc_list$hcvars),
-#            nranef = x$nranef)
-#   )
-#
-#
-#   # analysis_main  -------------------------------------------------------------
-#   if (analysis_main) {
-#     # betas
-#     if (is.null(betas)) betas <- TRUE
-#
-#     # sigma
-#     if ((any(families_main %in% c("gaussian", "Gamma", "lognorm")) |
-#          any(modeltypes_main %in% c("survreg"))) &
-#         is.null(sigma_main)) {
-#       sigma_main <- TRUE
-#     }
-#
-#     # tau
-#     if (any(families_main %in% c("gaussian", "Gamma", "lognorm", "beta")) &
-#         !isFALSE(tau_main)) {
-#       tau_main <- TRUE
-#     }
-#
-#     # sigma_main
-#     sigvars <- if (isTRUE(sigma_main)) {
-#       names(list_main)[families_main %in% c("gaussian", "Gamma", "lognorm")]
-#     }
-#
-#     # tau_main
-#     tauvars <- if (isTRUE(tau_main)) {
-#       setdiff(
-#         names(list_main)[families_main %in% c("gaussian", "Gamma",
-#                                               "lognorm", "beta")],
-#         sigvars
-#       )
-#     }
-#
-#
-#
-#     # gamma
-#     gamma_main <- any(modeltypes_main %in% c("clmm", "clm")) &
-#       !isFALSE(gamma_main)
-#
-#
-#     # basehaz
-#     if (any(modeltypes_main %in% c("coxph", "JM")) & !isFALSE(basehaz))
-#       # for a cox model with no betas, something needs to be monitored to
-#       # prevent JAGS error "No valid monitors set".
-#       basehaz <- TRUE
-#
-#     # D
-#     if (!isFALSE(D_main))
-#       D_main <- TRUE
-#   }
-#
-#
-#   # analysis_random ------------------------------------------------------------
-#   if (analysis_random &
-#       any(modeltypes_main %in% c("glmm", "clmm", "mlogitmm", "coxph",
-#                                  "survreg", "JM"))) {
-#     if (is.null(ranef_main)) ranef_main <- TRUE
-#     if (is.null(invD_main)) invD_main <- TRUE
-#     if (is.null(D_main)) D_main <- TRUE
-#     if (is.null(RinvD_main)) RinvD_main <- TRUE
-#   }
-#
-#   # imputation parameters -----------------------------------------------------
-#   if (other_models) {
-#     if (length(setdiff(names(info_list), names(Mlist$fixed))) == 0) {
-#       if (mess)
-#         msg("There are no missing values in covariates. I will set
-#             "other_models = FALSE".")
-#       other_models <- FALSE
-#     } else {
-#       if (is.null(alphas)) alphas <- TRUE
-#       if (is.null(tau_other)) tau_other <- TRUE
-#       if (is.null(gamma_other)) gamma_other <- TRUE
-#       if (is.null(delta_other)) delta_other <- TRUE
-#       if (is.null(D_other)) D_other <- TRUE
-#     }
-#   }
-#
-#   # other ----------------------------------------------------------------------
-#   arglist <- mget(names(formals()), sys.frame(sys.nframe()))
-#
-#   for (i in names(arglist)) {
-#     if (is.null(arglist[[i]]) & i != "other") assign(i, FALSE)
-#   }
-#
-#   # params ---------------------------------------------------------------------
-#   params <- c(
-#     # beta
-#     if (isTRUE(betas) &
-#         any(grepl("^beta\\b", do.call(rbind, get_coef_names(info_list))$coef)))
-#       "beta",
-#
-#     # basehaz
-#     if (isTRUE(basehaz))
-#       paste0("beta_Bh0_",
-#              sapply(list_main[modeltypes_main %in% c("coxph", "JM")],
-#                     "[[", "varname")),
-#     # gamma_main
-#     if (isTRUE(gamma_main))
-#       paste0("gamma_", names(list_main)[modeltypes_main %in% c("clm", "clmm")]),
-#
-#     # delta_main
-#     if (isTRUE(delta_main))
-#       paste0("delta_", names(list_main)[modeltypes_main %in% c("clm", "clmm")]),
-#
-#     # sigma_main
-#     if (isTRUE(sigma_main)) {
-#       c(if (length(sigvars) > 0) paste0("sigma_", sigvars),
-#
-#         if (any(modeltypes_main %in% c("survreg")))
-#           paste0("shape_", sapply(list_main[modeltypes_main %in% c("survreg")],
-#                                   "[[", "varname"))
-#       )
-#     },
-#
-#     # tau_main
-#     if (isTRUE(tau_main && length(tauvars) > 0))
-#       paste0("tau_", tauvars),
-#
-#
-#
-#     # random effects parameters
-#     if (any(!sapply(ranef_info_main, is.null))) {
-#       c(
-#         # ranef_main
-#         if (isTRUE(ranef_main))
-#           sapply(ranef_info_main, function(k)
-#             paste0("b_", k$varname, "_", k$lvls)),
-#
-#         # invD_main
-#         if (isTRUE(invD_main))
-#           unlist(sapply(ranef_info_main, function(x)
-#             sapply(x$lvls, function(lvl) {
-#               sapply(1L:max(1L, x$nranef[lvl]), function(i)
-#                 paste0("invD_", x$varname, "_", lvl, "[", 1L:i, ",", i, "]")
-#               )}))),
-#
-#         # D_main
-#         if (isTRUE(D_main))
-#           unlist(sapply(ranef_info_main, function(x)
-#             sapply(x$lvls, function(lvl) {
-#               sapply(1L:max(1L, x$nranef[lvl]), function(i)
-#                 paste0("D_", x$varname, "_", lvl, "[", 1L:i, ",", i, "]")
-#               )}))),
-#
-#         # RinvD_main
-#         if (isTRUE(RinvD_main))
-#           unlist(sapply(ranef_info_main, function(x)
-#             sapply(x$lvls, function(lvl) {
-#             paste0("RinvD_", x$varname, "_", lvl, "[", 1L:max(1L, x$nranef[lvl]),
-#                    ",", 1L:max(1L, x$nranef[lvl]), "]")
-#             })))
-#       )
-#     },
-#
-#     # alphas
-#     if (isTRUE(alphas)) "alpha",
-#
-#     # tau_other
-#     if (isTRUE(tau_other) & any(families_other %in%
-#                                 c("gaussian", "lognorm", "gamma", "beta")))
-#       paste0("tau_", names(list_other)[
-#         families_other %in% c("gaussian", "lognorm", "gamma", "beta")]),
-#
-#     # gamma_other
-#     if (isTRUE(gamma_other) &
-#         any(modeltypes_other %in% c("clm", "clmm")))
-#       paste0("gamma_", names(list_other)[
-#         modeltypes_other %in% c("clm", "clmm")]),
-#
-#     # delta_other
-#     if (isTRUE(delta_other) & any(modeltypes_other %in% c("clm", "clmm")))
-#       paste0("delta_", names(list_other)[
-#         modeltypes_other %in% c("clm", "clmm")]),
-#
-#     # random effects in other models
-#     if (any(!sapply(ranef_info_other, is.null))) {
-#       c(
-#         # ranef_other
-#         if (isTRUE(ranef_other))
-#           sapply(ranef_info_other, function(k)
-#             paste0("b_", k$varname, "_", k$lvls)),
-#
-#         # invD_other
-#         if (isTRUE(invD_other))
-#           unlist(sapply(ranef_info_other, function(x)
-#             sapply(x$lvls, function(lvl) {
-#               sapply(1L:max(1L, x$nranef[lvl]), function(i)
-#                 paste0("invD_", x$varname, "_", lvl, "[", 1L:i, ",", i, "]")
-#               )}))),
-#
-#         # D_other
-#         if (isTRUE(D_other))
-#           unlist(sapply(ranef_info_other, function(x)
-#             sapply(x$lvls, function(lvl) {
-#               sapply(1L:max(1L, x$nranef[lvl]), function(i)
-#                 paste0("D_", x$varname, "_", lvl, "[", 1L:i, ",", i, "]")
-#               )}))),
-#
-#         # RinvD_other
-#         if (isTRUE(RinvD_other))
-#           unlist(sapply(ranef_info_other, function(x)
-#             sapply(x$lvls, function(lvl) {
-#               paste0("RinvD_", x$varname, "_", lvl, "[",
-#                      1L:max(1L, x$nranef[lvl]), ",", 1L:max(1L, x$nranef[lvl]), "]")
-#             })))
-#       )
-#     },
-#
-#     # other
-#     other,
-#
-#     # imps
-#     if (isTRUE(imps)) {
-#       unlist(unname(
-#         sapply(names(Mlist$M), function(k) {
-#         if (any(is.na(Mlist$M[[k]]))) {
-#           M_NA <- which(is.na(Mlist$M[[k]][, colnames(Mlist$M[[k]]) %in%
-#                                              names(Mlist$data), drop = FALSE]),
-#                         arr.ind = TRUE)
-#
-#           apply(M_NA, 1L, function(x) {
-#             paste0(k, "[", x[1L], ",", x[2L], "]")
-#           })
-#         }
-#       }, simplify = FALSE)
-#       ))
-#     }
-#   )
-#   unname(params)
-# }
