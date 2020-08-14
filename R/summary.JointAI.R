NErler/JointAI

commit eb1b32760aefaaf41f5157c7e0d7eeb2838bfc18

bugfix: the warnings attribute of the random effects variance covariance matrix is a list, so in the check if warnings need to be printed need to unlist before using is.null()

R/summary.JointAI.R
@@ -270,12 +270,12 @@ print.summary.JointAI <- function(x, digits = max(3, .Options$digits - 4),
         warnings <- attr(x$res[[k]]$rd_vcov, "warnings")
         attr(x$res[[k]]$rd_vcov, "warnings") <- NULL
         print(x$res[[k]]$rd_vcov, digits = digits, na.print = "")
-        if (!is.null(warnings)) {
+        if (!is.null(unlist(warnings))) {
           warnmsg(warnings)
         }
       }
 
-      if (!is.null(x$res[[k]]$sigma)) {
+if (!is.null(x$randomes[[k]]$sigma))  {
         cat("\nPosterior summary of residual std. deviation:\n")
         print(x$res[[k]]$sigma, digits = digits, na.print = "")
       }

commit 3cd06ef55702a6924013f868b6b18f2568d00e7f

bugfix: fixed typo that was introduced in eb1b32760a

R/summary.JointAI.R
@@ -275,7 +275,7 @@ print.summary.JointAI <- function(x, digits = max(3, .Options$digits - 4),
         }
       }
 
-if (!is.null(x$randomes[[k]]$sigma))  {
+      if (!is.null(x$res[[k]]$sigma))  {
         cat("\nPosterior summary of residual std. deviation:\n")
         print(x$res[[k]]$sigma, digits = digits, na.print = "")
       }
@@ -617,7 +617,7 @@ get_missinfo <- function(object) {
 
   complcases <- lapply(names(groups), function(k) {
     cc <- complete.cases(object$data[match(unique(groups[[k]]), groups[[k]]),
-                               names(data_lvls[data_lvls == k])])
+                                     names(data_lvls[data_lvls == k])])
 
     as.data.frame(
       Filter(Negate(is.null),