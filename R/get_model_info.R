395d749807d61dd2230e8b2691b801f9bfb3b4ea
Nicole Erler <n.erler@erasmusmc.nl>
2020-10-26 11:28:23 +0100

    only set association structure for covariates of survival part

--- R/get_model_info.R
+++ R/get_model_info.R
@@ -222,7 +222,8 @@ get_model1_info <- function(k, Mlist, par_index_main, par_index_other,
     refs = Mlist$refs[[k]],
     covnames = covnames,
     assoc_type  = if (modeltype %in% "JM") {
-      get_assoc_type(tvars, Mlist$models, assoc_type, Mlist$refs)
+      get_assoc_type(intersect(tvars, names(unlist(unname(lp)))),
+                     Mlist$models, assoc_type, Mlist$refs)
     } else if (modeltype %in% "coxph") {
       "obs.value"
     },
