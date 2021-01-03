45ac095b62fb8730984b10e4b1364a6a34e32fc1
Nicole Erler <n.erler@erasmusmc.nl>
2021-01-03 15:17:02 +0100

    fix issue in devel version with order() on a data.frame

--- R/helpfunctions_divide_matrices.R
+++ R/helpfunctions_divide_matrices.R
@@ -126,7 +126,7 @@ fill_locf <- function(data, fixed, random, auxvars, timevar, groups) {
   # (needed because otherwise "groups" would not fit any more after sorting)
   # and sort the data by the time variable
   data$rowiddd <- seq_len(nrow(data))
-  data <- data[order(data[timevar]), ]
+  data <- data[order(data[[timevar]]), ]
 
   # split the data by patient, and fill in values in the time-varying variables
   datlist <- lapply(split(data, data[surv_lvl]), function(x) {
