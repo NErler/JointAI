bf3723f662dd92f4d60f7a710244e0714c45c4c4
Nicole Erler <n.erler@erasmusmc.nl>
2022-02-06 11:19:59 +0100

    move functions from helpfunctions_JAGSmodel.R to helpfunctions_print-paste.R

--- R/helpfunctions_JAGSmodel.R
+++ R/helpfunctions_JAGSmodel.R
@@ -1,62 +1,4 @@
 # help functions ---------------------------------------------------------------
-tab <- function(times = 2L) {
-  # creates a vector of spaces to facilitate indentation
-
-  tb <- " "
-  paste(rep(tb, times), collapse = "")
-}
-
-
-add_dashes <- function(x, width = 95L) {
-  # add separation lines between sub-models in JAGS model for readability
-  # - x: name of the sub-model
-
-  paste(x, paste0(rep("-", 80L - nchar(x)), collapse = ""))
-}
-
-
-add_linebreaks <- function(string, indent, width = 90L) {
-  # add linebreaks to a string, breaking it after a "+" sign
-  # - string: the linear predictor string to be broken
-  # - indent: in case of a linebreak, how much should the new line be indented?
-  # - width: output width
-
-  if (is.null(string)) {
-    return(NULL)
-  }
-
-  # identify position of "+"
-  m <- gregexpr(" \\+ ", string)[[1L]]
-
-  # if there is no "+", return the original string
-  if (all(m < 0L)) {
-    return(string)
-  }
-
-  # calculate the lengths of the sub-strings
-  len <- c(as.numeric(m)[1L], diff(c(as.numeric(m), nchar(string))))
-
-
-  # check how many sub-strings (and the indent) can be combined until reaching
-  # the maximal width, and create a string of " + " (no break) and
-  # " +\n" (break) to be pasted in afterwards
-  # (there is probably a more elegant way to do this)
-  i <- 1L
-  br <- character(0L)
-  while (i < length(len)) {
-    cs <- cumsum(len[i:length(len)])
-    nfit <- max(1L, which(cs <= (width - indent)))
-    br <- c(
-      br, rep(" + ", nfit - 1L),
-      if ((i + nfit - 1L) < length(len)) paste0(" +\n", tab(indent))
-    )
-    i <- i + nfit
-  }
-
-  paste0(strsplit(string, " \\+ ")[[1L]], c(br, ""), collapse = "")
-}
-
-
 
 # linear predictors ------------------------------------------------------------
 paste_linpred <- function(parname, parelmts, matnam, index, cols, scale_pars,
@@ -1283,6 +1225,4 @@ paste_ps <- function(nr, env = parent.frame()) {
   paste0("psum", if (gk) "gk", "_", vn, "[", ind, ", ", nr, if (gk) ", k", "]")
 }
 
-minmax <- function(x, max = "1-1e-10", min = "1e-10") {
-  paste0("max(", min, ", min(", max, ", ", x, "))")
-}
+
