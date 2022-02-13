7cb9aa4162825d98df9eba4289b78a3a0a322d19
Nicole Erler <n.erler@erasmusmc.nl>
2022-02-13 12:50:02 +0100

    remove doFuture from test for add_samples

--- tests/testthat/test-add_samples.R
+++ tests/testthat/test-add_samples.R
@@ -10,7 +10,6 @@ test_that('add_samples works in simple setting',{
 
 test_that('add_samples works in parallel', {
   skip_on_os(c("windows", "mac"))
-  doFuture::registerDoFuture()
   future::plan(future::cluster, workers = 2)
   lm2 <- lm_imp(y ~ C1 + C2 + B2, data = wideDF, n.iter = 50)
   expect_s3_class(add_samples(lm2, add = TRUE, n.iter = 50), class = "JointAI")
