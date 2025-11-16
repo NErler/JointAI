3d47dd0656ebe0416232bc59ca9ba5ed6f05e766
Nicole Erler <NErler@users.noreply.github.com>
2025-11-16 16:01:50 +0100

    Clean-up and refactoring

--- R/helpfunctions_checks.R
+++ R/helpfunctions_checks.R
@@ -1,14 +1,17 @@
-
 # other functions --------------------------------------------------------------
 #TODO: document this function
 #TODO: check this function
-prep_arglist <- function(analysis_type, family = NULL, formals = formals(),
-                         call = match.call(), sframe = sys.frame(sys.nframe())) {
+prep_arglist <- function(
+  analysis_type,
+  family = NULL,
+  formals = formals(),
+  call = match.call(),
+  sframe = sys.frame(sys.nframe())
+) {
   arglist <- mget(names(formals), sframe)
 
   thiscall <- as.list(call)[-1L]
-  arglist <- c(arglist,
-               thiscall[!names(thiscall) %in% names(arglist)])
+  arglist <- c(arglist, thiscall[!names(thiscall) %in% names(arglist)])
 
   arglist$thecall <- call
 
@@ -21,10 +24,13 @@ prep_arglist <- function(analysis_type, family = NULL, formals = formals(),
     arglist$thecall$fixed <- arglist$fixed
   }
 
-
-  if (!inherits(arglist$data, 'data.frame'))
-    errormsg("Please provide a %s to the argument %s.",
-             sQuote('data.frame'), dQuote('data'))
+  if (!inherits(arglist$data, 'data.frame')) {
+    errormsg(
+      "Please provide a %s to the argument %s.",
+      sQuote('data.frame'),
+      dQuote('data')
+    )
+  }
 
   # analysis type
   arglist$analysis_type <- analysis_type
@@ -40,9 +46,12 @@ prep_arglist <- function(analysis_type, family = NULL, formals = formals(),
       thefamily <- family
     }
 
-    if (!thefamily$link %in%
-        c("identity", "log", "logit", "probit", "log", "cloglog", "inverse"))
+    if (
+      !thefamily$link %in%
+        c("identity", "log", "logit", "probit", "log", "cloglog", "inverse")
+    ) {
       errormsg("%s is not an allowed link function.", dQuote(thefamily$link))
+    }
 
     attr(arglist$analysis_type, "family") <- thefamily
   }
@@ -68,7 +77,6 @@ prep_arglist <- function(analysis_type, family = NULL, formals = formals(),
 #TODO: document this function
 #TODO: check this function
 check_fixed_random <- function(arglist) {
-
   # if there is a "fixed" effects formula, but no "random" , check if "fixed"
   # contains the fixed and random effects
   if (!is.null(arglist$fixed) & is.null(arglist$random)) {
@@ -83,24 +91,28 @@ check_fixed_random <- function(arglist) {
     can_split <- try(split_formula_list(arglist$formula))
 
     if (inherits(can_split, 'try-error')) {
-      errormsg("I cannot split the %s into a fixed and random effects part.",
-               dQuote("formula"))
+      errormsg(
+        "I cannot split the %s into a fixed and random effects part.",
+        dQuote("formula")
+      )
     } else if (is.null(can_split$random[[1]])) {
-      errormsg("I cannot extract a random effects formula from %s.",
-               dQuote("formula"))
+      errormsg(
+        "I cannot extract a random effects formula from %s.",
+        dQuote("formula")
+      )
     }
   }
 
-
-  if (is.null(arglist$fixed) & length(arglist$formula) == 0)
+  if (is.null(arglist$fixed) & length(arglist$formula) == 0) {
     errormsg("No fixed effects structure specified.")
+  }
 
-  if (is.null(arglist$random) & length(arglist$formula) == 0)
+  if (is.null(arglist$random) & length(arglist$formula) == 0) {
     errormsg("No random effects structure specified.")
+  }
 
   arglist
 }
-
 
 
 #' Check that all variables in formulas are in the data
@@ -115,9 +127,13 @@ check_fixed_random <- function(arglist) {
 #' @keywords internal
 #' used in model_imp (2020-06-09)
 #'
-check_vars_in_data <- function(datanames, fixed = NULL, random = NULL,
-                               auxvars = NULL, timevar = NULL) {
-
+check_vars_in_data <- function(
+  datanames,
+  fixed = NULL,
+  random = NULL,
+  auxvars = NULL,
+  timevar = NULL
+) {
   # make vector of any variable occurring in the formulas
   allvars <- all_vars(fixed, random, auxvars, timevar)
 
@@ -151,24 +167,26 @@ check_vars_in_data <- function(datanames, fixed = NULL, random = NULL,
 #' @returns nothing, but throws an error if a variable is of an unknown class
 #' @keywords internal
 #'
-check_classes <- function(data,
-                          fixed = NULL,
-                          random = NULL,
-                          auxvars = NULL,
-                          timevar = NULL,
-                          mess = TRUE) {
-
+check_classes <- function(
+  data,
+  fixed = NULL,
+  random = NULL,
+  auxvars = NULL,
+  timevar = NULL,
+  mess = TRUE
+) {
   vars <- all_vars(fixed, remove_grouping(random), auxvars, timevar)
   classes <- unlist(sapply(data[vars], class))
   known_classes <- c("numeric", "ordered", "factor", "logical", "integer")
 
   # Throw an error for variables of unknown classes
   if (any(w <- which(!classes %in% known_classes))) {
-
     unknown_classes <- sapply(split(classes[w], classes[w]), function(x) {
       paste0(
         dQuote(unique(x)),
-        " (variables: ", paste0(names(x), collapse = ", "), ")"
+        " (variables: ",
+        paste0(names(x), collapse = ", "),
+        ")"
       )
     })
 
@@ -178,7 +196,6 @@ check_classes <- function(data,
     )
   }
 }
-
 
 
 #' Check for empty factor levels
@@ -201,7 +218,6 @@ check_classes <- function(data,
 #' @keywords internal
 #'
 drop_levels <- function(data, allvars, warn = TRUE) {
-
   data_orig <- data
   data[allvars] <- droplevels(data[allvars])
 
@@ -209,14 +225,16 @@ drop_levels <- function(data, allvars, warn = TRUE) {
     lvl1 <- sapply(data_orig[allvars], function(x) length(levels(x)))
     lvl2 <- sapply(data[allvars], function(x) length(levels(x)))
 
-    if (any(lvl1 != lvl2))
-      warnmsg('The variable(s) %s has/have empty levels.
+    if (any(lvl1 != lvl2)) {
+      warnmsg(
+        'The variable(s) %s has/have empty levels.
               Use `droplevels()` on your input data to remove empty levels.',
-          paste_and(dQuote(names(lvl1)[which(lvl1 != lvl2)])))
+        paste_and(dQuote(names(lvl1)[which(lvl1 != lvl2)]))
+      )
+    }
   }
   return(data)
 }
-
 
 
 #' Replace ":" with "_" in a string
@@ -255,7 +273,6 @@ clean_names <- function(string) {
 #' @keywords internal
 #'
 convert_variables <- function(data, allvars, mess = TRUE) {
-
   data_orig <- data
 
   for (k in allvars) {
@@ -275,7 +292,6 @@ convert_variables <- function(data, allvars, mess = TRUE) {
 
   return(data)
 }
-
 
 
 #' Replace NaN values with NA
@@ -316,30 +332,35 @@ two_value_to_factor <- function(x) {
 #' @keywords internal
 #'
 compare_data_structure <- function(data1, data2) {
-
-  class_change <- mapply(function(x1, x2) any(x1 != x2),
-                         x1 = lapply(data1, class),
-                         x2 = lapply(data2, class)
+  class_change <- mapply(
+    function(x1, x2) any(x1 != x2),
+    x1 = lapply(data1, class),
+    x2 = lapply(data2, class)
   )
   class_change = Filter(isTRUE, class_change)
 
-  level_change <- mapply(function(x1, x2) !isTRUE(all.equal(x1, x2)),
-                         x1 = lapply(data1, levels),
-                         x2 = lapply(data2, levels)
+  level_change <- mapply(
+    function(x1, x2) !isTRUE(all.equal(x1, x2)),
+    x1 = lapply(data1, levels),
+    x2 = lapply(data2, levels)
   )
   level_change <- Filter(isTRUE, level_change)
 
   if (any(class_change)) {
-    msg("The variable(s) %s was/were changed to %s.",
-        paste_and(dQuote(names(class_change))),
-        paste_and(dQuote(sapply(data2[names(class_change)], class))))
+    msg(
+      "The variable(s) %s was/were changed to %s.",
+      paste_and(dQuote(names(class_change))),
+      paste_and(dQuote(sapply(data2[names(class_change)], class)))
+    )
   }
 
   if (any(level_change)) {
     for (k in names(level_change)) {
-      msg("The levels of the variable %s was/were changed to %s.",
-          dQuote(k),
-          paste0(levels(data2[[k]]), collapse = ", "))
+      msg(
+        "The levels of the variable %s was/were changed to %s.",
+        dQuote(k),
+        paste0(levels(data2[[k]]), collapse = ", ")
+      )
     }
   }
 }
@@ -368,21 +389,29 @@ compare_data_structure <- function(data1, data2) {
 #' @keywords internal
 #'
 check_data <- function(data, fixed, random, auxvars, timevar, mess, warn) {
-
-  check_vars_in_data(names(data), fixed = fixed, random = random,
-                     auxvars = auxvars, timevar = timevar)
+  check_vars_in_data(
+    names(data),
+    fixed = fixed,
+    random = random,
+    auxvars = auxvars,
+    timevar = timevar
+  )
 
   check_classes(data, fixed = fixed, random = random, auxvars = auxvars)
 
-  data <- drop_levels(data = data,
-                      allvars = all_vars(fixed, random, auxvars),
-                      warn = warn)
+  data <- drop_levels(
+    data = data,
+    allvars = all_vars(fixed, random, auxvars),
+    warn = warn
+  )
 
   # convert variable with 2 different values (continuous, character or logical)
   # to factors
-  data <- convert_variables(data = data,
-                            allvars = all_vars(fixed, random, auxvars),
-                            mess = mess)
+  data <- convert_variables(
+    data = data,
+    allvars = all_vars(fixed, random, auxvars),
+    mess = mess
+  )
 
   data
 }
